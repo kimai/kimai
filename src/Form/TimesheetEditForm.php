ff48dd328ec80ea66a19c2a0c949ab112047bc05
Kevin Papst <kpapst@gmx.net>
2018-08-07 20:04:50 +0200

    Hotfix for Symfony Flex incompatibility with latest composer changes

--- src/Form/TimesheetEditForm.php
+++ src/Form/TimesheetEditForm.php
@@ -71,7 +71,6 @@ class TimesheetEditForm extends AbstractType
                 },
             ])
             ->add('description', TextareaType::class, [
-                'help' => 'timesheet.description.help',
                 'label' => 'label.description',
                 'required' => false,
             ])
