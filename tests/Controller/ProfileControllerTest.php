5b64161ab450f602ea651cc89cc9f938a97e9193
Kevin Papst <kpapst@gmx.net>
2018-08-08 22:35:41 +0200

    updated tests and docu

--- tests/Controller/ProfileControllerTest.php
+++ tests/Controller/ProfileControllerTest.php
@@ -10,6 +10,8 @@
 namespace App\Tests\Controller;
 
 use App\DataFixtures\UserFixtures;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
 
 /**
  * @coversDefaultClass \App\Controller\InvoiceController
@@ -45,5 +47,75 @@ class ProfileControllerTest extends ControllerBaseTest
         $client = $this->getClientForAuthenticatedUser();
         $this->request($client, '/profile/' . UserFixtures::USERNAME_TEAMLEAD);
         $this->assertFalse($client->getResponse()->isSuccessful());
+    }
+
+    public function testUpdateApiToken()
+    {
+        $client = $this->getClientForAuthenticatedUser(User::ROLE_USER);
+        $this->request($client, '/profile/' . UserFixtures::USERNAME_USER);
+
+        /** @var User $user */
+        $user = $client->getContainer()->get('security.token_storage')->getToken()->getUser();
+        /** @var EncoderFactoryInterface $passwordEncoder */
+        $passwordEncoder = $client->getContainer()->get('test.PasswordEncoder');
+
+        $this->assertTrue($passwordEncoder->getEncoder($user)->isPasswordValid($user->getApiToken(), UserFixtures::DEFAULT_API_TOKEN, $user->getSalt()));
+        $this->assertFalse($passwordEncoder->getEncoder($user)->isPasswordValid($user->getApiToken(), 'test123', $user->getSalt()));
+        $this->assertEquals(UserFixtures::USERNAME_USER, $user->getUsername());
+
+        $form = $client->getCrawler()->filter('form[name=user_api_token]')->form();
+        $client->submit($form, [
+            'user_api_token' => [
+                'plainApiToken' => [
+                    'first' => 'test123',
+                    'second' => 'test123',
+                ]
+            ]
+        ]);
+
+        $this->assertIsRedirect($client, $this->createUrl('/profile/' . urlencode(UserFixtures::USERNAME_USER)));
+        $client->followRedirect();
+        $this->assertTrue($client->getResponse()->isSuccessful());
+
+        $this->assertHasFlashSuccess($client);
+
+        $user = $client->getContainer()->get('security.token_storage')->getToken()->getUser();
+        $this->assertFalse($passwordEncoder->getEncoder($user)->isPasswordValid($user->getApiToken(), UserFixtures::DEFAULT_API_TOKEN, $user->getSalt()));
+        $this->assertTrue($passwordEncoder->getEncoder($user)->isPasswordValid($user->getApiToken(), 'test123', $user->getSalt()));
+    }
+
+    public function testUpdatePassword()
+    {
+        $client = $this->getClientForAuthenticatedUser(User::ROLE_USER);
+        $this->request($client, '/profile/' . UserFixtures::USERNAME_USER);
+
+        /** @var User $user */
+        $user = $client->getContainer()->get('security.token_storage')->getToken()->getUser();
+        /** @var EncoderFactoryInterface $passwordEncoder */
+        $passwordEncoder = $client->getContainer()->get('test.PasswordEncoder');
+
+        $this->assertTrue($passwordEncoder->getEncoder($user)->isPasswordValid($user->getPassword(), UserFixtures::DEFAULT_PASSWORD, $user->getSalt()));
+        $this->assertFalse($passwordEncoder->getEncoder($user)->isPasswordValid($user->getPassword(), 'test123', $user->getSalt()));
+        $this->assertEquals(UserFixtures::USERNAME_USER, $user->getUsername());
+
+        $form = $client->getCrawler()->filter('form[name=user_password]')->form();
+        $client->submit($form, [
+            'user_password' => [
+                'plainPassword' => [
+                    'first' => 'test123',
+                    'second' => 'test123',
+                ]
+            ]
+        ]);
+
+        $this->assertIsRedirect($client, $this->createUrl('/profile/' . urlencode(UserFixtures::USERNAME_USER)));
+        $client->followRedirect();
+        $this->assertTrue($client->getResponse()->isSuccessful());
+
+        $this->assertHasFlashSuccess($client);
+
+        $user = $client->getContainer()->get('security.token_storage')->getToken()->getUser();
+        $this->assertFalse($passwordEncoder->getEncoder($user)->isPasswordValid($user->getPassword(), UserFixtures::DEFAULT_PASSWORD, $user->getSalt()));
+        $this->assertTrue($passwordEncoder->getEncoder($user)->isPasswordValid($user->getPassword(), 'test123', $user->getSalt()));
     }
 }
