6b0db2c2236ecfc775df584f8df9fcf8d6dd2284
Kevin Papst <kpapst@gmx.net>
2018-09-06 12:30:56 +0200

    updated unit test

--- tests/Entity/ProjectTest.php
+++ tests/Entity/ProjectTest.php
@@ -9,6 +9,8 @@
 
 namespace App\Tests\Entity;
 
+use App\Entity\Activity;
+use App\Entity\Customer;
 use App\Entity\Project;
 
 /**
@@ -29,6 +31,35 @@ class ProjectTest extends AbstractEntityTest
         // activities
         $this->assertNull($sut->getFixedRate());
         $this->assertNull($sut->getHourlyRate());
+    }
+
+
+    public function testSetterAndGetter()
+    {
+        $sut = new Project();
+
+        $customer = (new Customer())->setName('customer');
+        $this->assertInstanceOf(Project::class, $sut->setCustomer($customer));
+        $this->assertSame($customer, $sut->getCustomer());
+
+        $this->assertInstanceOf(Project::class, $sut->setName('123456789'));
+        $this->assertEquals('123456789', (string)$sut);
+
+        $this->assertInstanceOf(Project::class, $sut->setOrderNumber('123456789'));
+        $this->assertEquals('123456789', $sut->getOrderNumber());
+
+        $this->assertInstanceOf(Project::class, $sut->setComment('a comment'));
+        $this->assertEquals('a comment', $sut->getComment());
+
+        $this->assertInstanceOf(Project::class, $sut->setVisible(false));
+        $this->assertFalse($sut->getVisible());
+
+        $this->assertInstanceOf(Project::class, $sut->setBudget(12345.67));
+        $this->assertEquals(12345.67, $sut->getBudget());
+
+        $activities = [(new Activity())->setName('foo')];
+        $this->assertInstanceOf(Project::class, $sut->setActivities($activities));
+        $this->assertSame($activities, $sut->getActivities());
 
         $this->assertInstanceOf(Project::class, $sut->setFixedRate(13.47));
         $this->assertEquals(13.47, $sut->getFixedRate());
