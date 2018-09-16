bd48f918ad8e0e44b0103586cb8418589fb0d3dd
Kevin Papst <kpapst@gmx.net>
2018-09-16 16:32:50 +0200

    added tests

---
+++ tests/Model/ProjectStatisticTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Model;
+
+use App\Model\ProjectStatistic;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \App\Model\ProjectStatistic
+ */
+class ProjectStatisticTest extends TestCase
+{
+    public function testDefaultValues()
+    {
+        $sut = new ProjectStatistic();
+        $this->assertEquals(0, $sut->getActivityAmount());
+        $this->assertEquals(0, $sut->getCount());
+        $this->assertEquals(0, $sut->getRecordAmount());
+        $this->assertEquals(0, $sut->getRecordDuration());
+    }
+
+    public function testSetter()
+    {
+        $sut = new ProjectStatistic();
+        $sut->setCount(123);
+        $sut->setRecordAmount(7654.298);
+        $sut->setRecordDuration(826.10);
+        $sut->setActivityAmount(13);
+
+        $this->assertEquals(13, $sut->getActivityAmount());
+        $this->assertEquals(123, $sut->getCount());
+        $this->assertEquals(7654, $sut->getRecordAmount());
+        $this->assertEquals(826, $sut->getRecordDuration());
+    }
+}
