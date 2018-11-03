a9f419869d8aeadb3b7a7835df5dc36751775792
Kevin Papst <kpapst@gmx.net>
2018-11-03 03:26:50 +0100

    migration update

--- src/Migrations/Version20181031220003.php
+++ src/Migrations/Version20181031220003.php
@@ -43,7 +43,7 @@ final class Version20181031220003 extends AbstractMigration
             $this->addSql('CREATE INDEX IDX_4F60C6B181C06096 ON ' . $timesheet . ' (activity_id)');
             $this->addSql('CREATE INDEX IDX_4F60C6B1166D1F9C ON ' . $timesheet . ' (project_id)');
         } else {
-            $this->addSql('ALTER TABLE ' . $timesheet . ' ADD project_id INT DEFAULT NULL AFTER activity_id, CHANGE user user INT DEFAULT NULL, CHANGE activity_id activity_id INT DEFAULT NULL, CHANGE end_time end_time DATETIME DEFAULT NULL, CHANGE duration duration INT DEFAULT NULL, CHANGE fixed_rate fixed_rate NUMERIC(10, 2) DEFAULT NULL, CHANGE hourly_rate hourly_rate NUMERIC(10, 2) DEFAULT NULL');
+            $this->addSql('ALTER TABLE ' . $timesheet . ' ADD project_id INT DEFAULT NULL AFTER activity_id, CHANGE user user INT DEFAULT NULL, CHANGE activity_id activity_id INT DEFAULT NULL, CHANGE duration duration INT DEFAULT NULL, CHANGE fixed_rate fixed_rate NUMERIC(10, 2) DEFAULT NULL, CHANGE hourly_rate hourly_rate NUMERIC(10, 2) DEFAULT NULL');
             $this->addSql('ALTER TABLE ' . $timesheet . ' ADD CONSTRAINT FK_4F60C6B1166D1F9C FOREIGN KEY (project_id) REFERENCES ' . $projects . ' (id) ON DELETE CASCADE');
             $this->addSql('CREATE INDEX IDX_4F60C6B1166D1F9C ON ' . $timesheet . ' (project_id)');
         }
@@ -75,7 +75,7 @@ final class Version20181031220003 extends AbstractMigration
         } else {
             $this->addSql('ALTER TABLE ' . $timesheet . ' DROP FOREIGN KEY FK_4F60C6B1166D1F9C');
             $this->addSql('DROP INDEX IDX_4F60C6B1166D1F9C ON ' . $timesheet);
-            $this->addSql('ALTER TABLE ' . $timesheet . ' DROP project_id, CHANGE user user INT DEFAULT NULL, CHANGE activity_id activity_id INT DEFAULT NULL, CHANGE end_time end_time DATETIME DEFAULT \'NULL\', CHANGE duration duration INT DEFAULT NULL, CHANGE fixed_rate fixed_rate NUMERIC(10, 2) DEFAULT \'NULL\', CHANGE hourly_rate hourly_rate NUMERIC(10, 2) DEFAULT \'NULL\'');
+            $this->addSql('ALTER TABLE ' . $timesheet . ' DROP project_id, CHANGE user user INT DEFAULT NULL, CHANGE activity_id activity_id INT DEFAULT NULL, CHANGE duration duration INT DEFAULT NULL, CHANGE fixed_rate fixed_rate NUMERIC(10, 2) DEFAULT \'NULL\', CHANGE hourly_rate hourly_rate NUMERIC(10, 2) DEFAULT \'NULL\'');
         }
     }
 }
