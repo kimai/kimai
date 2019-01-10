6460d30ebac699b4f0ba95115ee895ac70f999c9
Mathias Blasek <mathias.blasek@weblighthouse.de>
2019-01-10 06:38:18 +0100

    TagRepository erstellt

---
+++ src/Repository/TagRepository.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mathias
+ * Date: 2018-12-21
+ * Time: 07:47
+ */
+
+namespace App\Repository;
+
+/**
+ * Class TagRepository
+ *
+ * @package App\Repository
+ */
+class TagRepository extends AbstractRepository {
+
+}
