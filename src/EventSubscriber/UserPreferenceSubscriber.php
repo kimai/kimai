61055e7dc340ef613f2a0627c5db96ad1efd6ce5
Kevin Papst <kpapst@gmx.net>
2019-01-19 18:44:47 +0100

    fixes after merge

--- src/EventSubscriber/UserPreferenceSubscriber.php
+++ src/EventSubscriber/UserPreferenceSubscriber.php
@@ -20,6 +20,7 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TimezoneType;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Validator\Constraints\Range;
