4ca85aada432f1ba7f47cb801ab02efd3f2ed5ff
Mathias Blasek <mathias.blasek@weblighthouse.de>
2019-02-27 15:40:40 +0100

    removed foreign key statements

--- src/Migrations/Version20190226205245.php
+++ src/Migrations/Version20190226205245.php
@@ -35,8 +35,8 @@ class Version20190226205245 extends AbstractMigration
             $this->addSql('CREATE TABLE ' . $tags . ' (id INT(11) NOT NULL, tag_name VARCHAR(255) NOT NULL, PRIMARY KEY (id))');
         } else {
             $this->addSql('CREATE TABLE ' . $timesheetTags . ' (timesheet_id INT(11) NOT NULL, tag_id INT(11) NOT NULL, PRIMARY KEY (timesheet_id, tag_id)) DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ENGINE = InnoDB');
-            $this->addSql('ALTER TABLE ' . $timesheetTags . ' ADD CONSTRAINT FK_E3284EFEABDD46BE FOREIGN KEY(timesheet_id) REFERENCES ' . $timesheet . ' (id)');
-            $this->addSql('ALTER TABLE ' . $timesheetTags . ' ADD CONSTRAINT FK_E3284EFEBAD26311 FOREIGN KEY(tag_id) REFERENCES ' . $tags . ' (id)');
+            //$this->addSql('ALTER TABLE ' . $timesheetTags . ' ADD CONSTRAINT FK_E3284EFEABDD46BE FOREIGN KEY(timesheet_id) REFERENCES ' . $timesheet . ' (id)');
+            //$this->addSql('ALTER TABLE ' . $timesheetTags . ' ADD CONSTRAINT FK_E3284EFEBAD26311 FOREIGN KEY(tag_id) REFERENCES ' . $tags . ' (id)');
             $this->addSql('CREATE INDEX IDX_E3284EFEABDD46BE ON ' . $timesheetTags . ' (timesheet_id ASC)');
             $this->addSql('CREATE INDEX IDX_E3284EFEBAD26311 ON ' . $timesheetTags . ' (tag_id ASC)');
             $this->addSql('CREATE TABLE ' . $tags . ' (id INT(11) NOT NULL AUTO_INCREMENT, tag_name VARCHAR(255) NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ENGINE = InnoDB');
