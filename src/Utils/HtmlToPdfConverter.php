01f00be56e2070fecd3923fd65905620976a70ce
Kevin Papst <kpapst@gmx.net>
2019-03-22 12:37:39 +0100

    fix pdf export cache directory

---
+++ src/Utils/HtmlToPdfConverter.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Utils;
+
+interface HtmlToPdfConverter
+{
+    /**
+     * Returns the binary content of the PDF, which can be saved as file or send via Reponse.
+     * @param string $html
+     * @return mixed
+     */
+    public function convertToPdf(string $html);
+}
---
+++ src/Utils/MPdfConverter.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Utils;
+
+use Mpdf\Mpdf;
+use Mpdf\Output\Destination;
+
+class MPdfConverter implements HtmlToPdfConverter
+{
+    /**
+     * @var string
+     */
+    private $cacheDirectory;
+
+    public function __construct(string $cacheDirectory)
+    {
+        $this->cacheDirectory = $cacheDirectory;
+    }
+
+    /**
+     * @param string $html
+     * @return mixed|string
+     * @throws \Mpdf\MpdfException
+     */
+    public function convertToPdf(string $html)
+    {
+        $mpdf = new Mpdf([['tempDir' => $this->cacheDirectory]]);
+        $mpdf->WriteHTML($html);
+
+        return $mpdf->Output('', Destination::STRING_RETURN);
+    }
+}
