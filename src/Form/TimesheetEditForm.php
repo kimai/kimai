e990ae48003ff6dbeabe9077e710b65a65e8cb6b
Kevin Papst <kevinpapst@users.noreply.github.com>
2019-03-22 15:00:59 +0100

    fix modal autofocus problem, datepicker not shown on begin date (#653)

--- src/Form/TimesheetEditForm.php
+++ src/Form/TimesheetEditForm.php
@@ -111,9 +111,6 @@ class TimesheetEditForm extends AbstractType
                 'label' => 'label.begin',
                 'model_timezone' => $timezone,
                 'view_timezone' => $timezone,
-                'attr' => [
-                    'autofocus' => 'autofocus',
-                ],
             ]);
         }
 
