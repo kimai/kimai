0382d6b2ebb57eb4704ccfa879d0ff9f16926e80
Kevin Papst <kpapst@gmx.net>
2019-04-20 01:56:17 +0200

    apply users timezone to export date

--- src/Controller/ExportController.php
+++ src/Controller/ExportController.php
@@ -14,6 +14,7 @@ use App\Export\ServiceExport;
 use App\Form\Toolbar\ExportToolbarForm;
 use App\Repository\Query\ExportQuery;
 use App\Repository\TimesheetRepository;
+use App\Timesheet\UserDateTimeFactory;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -35,15 +36,20 @@ class ExportController extends AbstractController
      * @var ServiceExport
      */
     protected $export;
+    /**
+     * @var UserDateTimeFactory
+     */
+    protected $dateFactory;
 
     /**
      * @param TimesheetRepository $timesheet
      * @param ServiceExport $export
      */
-    public function __construct(TimesheetRepository $timesheet, ServiceExport $export)
+    public function __construct(TimesheetRepository $timesheet, ServiceExport $export, UserDateTimeFactory $dateTime)
     {
         $this->timesheetRepository = $timesheet;
         $this->export = $export;
+        $this->dateFactory = $dateTime;
     }
 
     /**
@@ -52,8 +58,8 @@ class ExportController extends AbstractController
      */
     protected function getDefaultQuery()
     {
-        $begin = new \DateTime('first day of this month');
-        $end = new \DateTime('last day of this month');
+        $begin = $this->dateFactory->createDateTime('first day of this month 00:00:00');
+        $end = $this->dateFactory->createDateTime('last day of this month 23:59:59');
 
         $query = new ExportQuery();
         $query->setOrder(ExportQuery::ORDER_ASC);
