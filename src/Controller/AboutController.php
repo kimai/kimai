abcb15a03de979c0c3ceb0043393d71f6391834d
Kevin Papst <kpapst@gmx.net>
2019-05-24 18:41:09 +0200

    removed cache clear from about screen

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -11,10 +11,6 @@ namespace App\Controller;
 
 use App\Constants;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
-use Symfony\Bundle\FrameworkBundle\Console\Application;
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Output\BufferedOutput;
-use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -43,11 +39,6 @@ class AboutController extends AbstractController
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function debugAction()
-    {
-        return $this->getAboutView();
-    }
-
-    protected function getAboutView(array $additional = [])
     {
         $phpInfo = $this->getPhpInfo();
         unset($phpInfo[0]);
@@ -89,8 +80,7 @@ class AboutController extends AbstractController
             ],
             'info' => $phpInfo,
             'settings' => $settings,
-            ],
-            $additional
+            ]
         ));
     }
 
@@ -117,28 +107,6 @@ class AboutController extends AbstractController
         return $this->render('about/license.html.twig', [
             'license' => $license
         ]);
-    }
-
-    /**
-     * @Route(path="/flush-cache", name="system_flush_cache", methods={"GET"})
-     *
-     * @Security("is_granted('system_actions')")
-     */
-    public function rebuildContainer(KernelInterface $kernel)
-    {
-        $application = new Application($kernel);
-        $application->setAutoExit(false);
-
-        $input = new ArrayInput([
-            'command' => 'cache:clear',
-            '--env' => $kernel->getEnvironment(),
-            '-n',
-        ]);
-
-        $output = new BufferedOutput();
-        $application->run($input, $output);
-
-        return $this->getAboutView(['content_action' => $output->fetch()]);
     }
 
     /**
