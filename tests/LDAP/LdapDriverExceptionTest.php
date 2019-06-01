4cff740a35498d51c9f0b55a0c42478b56fd1cc8
Kevin Papst <kpapst@gmx.net>
2019-06-01 13:20:47 +0200

    added tests

---
+++ tests/LDAP/LdapDriverExceptionTest.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Invoice;
+
+use App\Ldap\LdapDriverException;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \App\Ldap\LdapDriverException
+ */
+class LdapDriverExceptionTest extends TestCase
+{
+    public function testConstruct()
+    {
+        $sut = new LdapDriverException('Whooops');
+
+        self::assertInstanceOf(\Exception::class, $sut);
+
+        self::assertEquals('Whooops', $sut->getMessage());
+    }
+}
---
+++ tests/LDAP/SanitizingExceptionTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Invoice;
+
+use App\Ldap\SanitizingException;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \App\Ldap\SanitizingException
+ */
+class SanitizingExceptionTest extends TestCase
+{
+    public function testMessagesAreSanitized()
+    {
+        $ex = new \Exception('Could not find user foo with password bar in your LDAP');
+        $sut = new SanitizingException($ex, 'bar');
+
+        self::assertInstanceOf(\Exception::class, $sut);
+
+        self::assertNotContains('bar', $sut->getMessage());
+        self::assertNotContains('bar', (string) $sut);
+        self::assertEquals('Could not find user foo with password **** in your LDAP', $sut->getMessage());
+    }
+}
