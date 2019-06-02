97feed0be61fa61fa01264b00f37a57e62993074
Kevin Papst <kevinpapst@users.noreply.github.com>
2019-06-03 01:43:49 +0200

    updated frontend stack to latest versions (#729)

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,7 @@
 // - removing moment locales which are not used
 // - removing fullcalendar locales which are not used
 // - removing icheck which is not used
-// - removing jquery-ui which is not used?
+// - removing jquery-ui which is not used
 
 const $ = require('jquery');
 global.$ = global.jQuery = $;
@@ -37,35 +37,13 @@ require('../vendor/kevinpapst/adminlte-bundle/Resources/assets/admin-lte-extensi
 global.$.AdminLTE = {};
 global.$.AdminLTE.options = {};
 require('admin-lte/dist/js/adminlte.min');
-
-require('../vendor/kevinpapst/adminlte-bundle/Resources/assets/default_avatar.png');
-
 // ------------------- INLINED ADMIN-LTE DEFINITIONS -------------------
 // ---------------------------------------------------------------------
 
 require('./sass/app.scss');
 
-require('fullcalendar');
-require('fullcalendar/dist/gcal.min');
-require('fullcalendar/dist/locale/de');
-require('fullcalendar/dist/locale/it');
-require('fullcalendar/dist/locale/fr');
-require('fullcalendar/dist/locale/es');
-require('fullcalendar/dist/locale/ru');
-require('fullcalendar/dist/locale/ar');
-require('fullcalendar/dist/locale/hu');
-require('fullcalendar/dist/locale/pt-br');
-require('fullcalendar/dist/locale/sv');
-require('fullcalendar/dist/locale/ja');
-require('fullcalendar/dist/fullcalendar.min.css');
-
-// ------ for charts ------
-require('chart.js/dist/Chart.min');
-
 // ------ Kimai itself ------
 require('./js/KimaiWebLoader.js');
-require('./images/default_avatar.png');
-require('./images/signature.png');
 
-// ------ Autocomplete ------
+// ------ Autocomplete for tags only ------
 require('jquery-ui/ui/widgets/autocomplete');
