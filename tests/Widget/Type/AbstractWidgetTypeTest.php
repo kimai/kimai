653c38bad7034cd4814e20a9b55792ea5037984d
Kevin Papst <kpapst@gmx.net>
2019-06-12 18:21:30 +0200

    added widget tests

---
+++ tests/Widget/Type/AbstractWidgetTypeTest.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Widget\Type;
+
+use App\Widget\Type\AbstractWidgetType;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \App\Widget\Type\AbstractWidgetType
+ */
+abstract class AbstractWidgetTypeTest extends TestCase
+{
+    abstract public function createSut(): AbstractWidgetType;
+
+    public function testDefaultValues()
+    {
+        $sut = $this->createSut();
+        $this->assertInstanceOf(AbstractWidgetType::class, $sut);
+        $this->assertEquals('', $sut->getId());
+        $this->assertEquals('', $sut->getTitle());
+        $this->assertEquals(['dataType' => 'int'], $sut->getOptions());
+        $this->assertNull($sut->getData());
+        $this->assertEquals('bar', $sut->getOption('foo', 'bar'));
+    }
+
+    public function testFluentInterface()
+    {
+        $sut = $this->createSut();
+        $this->assertInstanceOf(AbstractWidgetType::class, $sut->setOptions([]));
+        $this->assertInstanceOf(AbstractWidgetType::class, $sut->setId(''));
+        $this->assertInstanceOf(AbstractWidgetType::class, $sut->setTitle(''));
+        $this->assertInstanceOf(AbstractWidgetType::class, $sut->setData(''));
+    }
+
+    public function testTitleViaOptionsFallback()
+    {
+        $sut = $this->createSut();
+        $sut->setTitle('bar');
+        $this->assertEquals('bar', $sut->getTitle());
+        $sut->setTitle('');
+        $this->assertEquals('', $sut->getTitle());
+        $sut->setOption('title', 'fooooo');
+        $this->assertEquals('fooooo', $sut->getTitle());
+    }
+
+    public function testSetter()
+    {
+        $sut = $this->createSut();
+
+        // options
+        $sut->setOption('föööö', 'trääääää');
+        $this->assertEquals('trääääää', $sut->getOption('föööö', 'tröööö'));
+        $this->assertEquals('trääääää', $sut->getOption('föööö', 'tröööö'));
+        $this->assertEquals(['dataType' => 'int', 'föööö' => 'trääääää'], $sut->getOptions());
+
+        $sut->setOptions(['blub' => 'blab', 'dataType' => 'money']);
+        $this->assertEquals(['blub' => 'blab', 'dataType' => 'money', 'föööö' => 'trääääää'], $sut->getOptions());
+
+        // id
+        $sut->setId('cvbnmyx');
+        $this->assertEquals('cvbnmyx', $sut->getId());
+
+        // data
+        $sut->setData('slkudfhalksjdhfkljsahdf');
+        $this->assertEquals('slkudfhalksjdhfkljsahdf', $sut->getData());
+
+        $data = new \stdClass();
+        $sut->setData($data);
+        $this->assertSame($data, $sut->getData());
+    }
+}
---
+++ tests/Widget/Type/CounterTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Widget\Type;
+
+use App\Widget\Type\AbstractWidgetType;
+use App\Widget\Type\Counter;
+use App\Widget\Type\SimpleWidget;
+
+/**
+ * @covers \App\Widget\Type\Counter
+ */
+class CounterTest extends AbstractWidgetTypeTest
+{
+    public function createSut(): AbstractWidgetType
+    {
+        return new Counter();
+    }
+
+    public function testExtendsSimpleWidget()
+    {
+        $sut = $this->createSut();
+        $this->assertInstanceOf(SimpleWidget::class, $sut);
+    }
+}
