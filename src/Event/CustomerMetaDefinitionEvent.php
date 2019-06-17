ae1c86d7fbdb0fd5557ad0cb9217e422d74e307e
Kevin Papst <kpapst@gmx.net>
2019-06-18 00:03:09 +0200

    new events

---
+++ src/Event/CustomerMetaDefinitionEvent.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Event;
+
+use App\Entity\Customer;
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * This event can be used, to dynamically add meta fields to customers
+ */
+class CustomerMetaDefinitionEvent extends Event
+{
+    /**
+     * @var Customer
+     */
+    protected $entity;
+
+    public function __construct(Customer $entity)
+    {
+        $this->entity = $entity;
+    }
+
+    public function getEntity(): Customer
+    {
+        return $this->entity;
+    }
+}
