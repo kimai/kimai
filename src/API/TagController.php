23f42d24d387b17bb05189dc1f5d61cfd9d352a7
Kevin Papst <kpapst@gmx.net>
2019-09-04 18:20:51 +0200

    do not limit character for tag search

--- src/API/TagController.php
+++ src/API/TagController.php
@@ -58,7 +58,7 @@ class TagController extends BaseApiController
      *      )
      * )
      *
-     * @Rest\QueryParam(name="name", requirements="[a-zA-Z0-9 -\.]+", strict=true, nullable=true, description="Search term to filter tag list")
+     * @Rest\QueryParam(name="name", strict=true, nullable=true, description="Search term to filter tag list")
      *
      * @return Response
      */
