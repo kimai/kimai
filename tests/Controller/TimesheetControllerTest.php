35c65ba890c6296afc01547996acc2fa9a3b0b14
Kevin Papst <kevinpapst@users.noreply.github.com>
2019-09-13 17:16:53 +0200

    composer update (#1104)

--- tests/Controller/TimesheetControllerTest.php
+++ tests/Controller/TimesheetControllerTest.php
@@ -144,7 +144,9 @@ class TimesheetControllerTest extends ControllerBaseTest
         $this->assertTrue($client->getResponse()->isSuccessful());
 
         $node = $client->getCrawler()->filter('body');
-        $this->assertEquals('invoice_print', $node->getNode(0)->getAttribute('class'));
+        /** @var \DOMElement $body */
+        $body = $node->getNode(0);
+        $this->assertEquals('invoice_print', $body->getAttribute('class'));
 
         $result = $node->filter('section.invoice table.table tbody tr');
         $this->assertEquals(5, count($result));
