3d1f3ead9bf6f03276db300507d2a2bc56810775
Kevin Papst <kpapst@gmx.net>
2019-09-17 18:51:28 +0200

    updated test

--- src/Entity/MetaTableTypeInterface.php
+++ src/Entity/MetaTableTypeInterface.php
@@ -139,5 +139,13 @@ interface MetaTableTypeInterface
      *
      * @return string
      */
-    public function getLabel(): string;
+    public function getLabel(): ?string;
+    
+    /**
+     * Sets the label shown to the end-user.
+     *
+     * @param string $label
+     * @return MetaTableTypeInterface
+     */
+    public function setLabel(string $label): MetaTableTypeInterface;
 }
