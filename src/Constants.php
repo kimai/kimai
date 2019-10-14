de533e7031ae194b35f6a638cd95f5c94c516796
Kevin Papst <kevinpapst@users.noreply.github.com>
2019-10-14 12:30:11 +0200

    Kimai 1.4.2 (#1167)

--- src/Constants.php
+++ src/Constants.php
@@ -17,7 +17,7 @@ class Constants
     /**
      * The current release version
      */
-    public const VERSION = '1.4';
+    public const VERSION = '1.4.2';
     /**
      * The current release status, either "stable" or "dev"
      */
