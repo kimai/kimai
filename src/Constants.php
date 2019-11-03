140fa4b7c0bf2fd4b9111685f4c54e8cd2168d55
Kevin Papst <kevinpapst@users.noreply.github.com>
2019-11-03 17:31:33 +0100

    prepare release 1.5 (#1214)

--- src/Constants.php
+++ src/Constants.php
@@ -17,7 +17,7 @@ class Constants
     /**
      * The current release version
      */
-    public const VERSION = '1.4.2';
+    public const VERSION = '1.5';
     /**
      * The current release status, either "stable" or "dev"
      */
