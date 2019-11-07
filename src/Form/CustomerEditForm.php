87c0fbb6d2f648e0f802944c625f7d80da8b79d2
Kevin Papst <kpapst@gmx.net>
2019-11-07 13:06:03 +0100

    loose email validation on customer

--- src/Form/CustomerEditForm.php
+++ src/Form/CustomerEditForm.php
@@ -10,10 +10,10 @@
 namespace App\Form;
 
 use App\Entity\Customer;
+use App\Form\Type\MailType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\Form\Extension\Core\Type\CurrencyType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -84,8 +84,7 @@ class CustomerEditForm extends AbstractType
                 'required' => false,
                 'attr' => ['icon' => 'mobile'],
             ])
-            ->add('email', EmailType::class, [
-                'label' => 'label.email',
+            ->add('email', MailType::class, [
                 'required' => false,
             ])
             ->add('homepage', UrlType::class, [
