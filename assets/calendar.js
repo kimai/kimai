aaeb97ea0d62ac622d7a5249199af846c593b9b2
Badgy <jonaskhansen@gmail.com>
2019-11-30 22:54:27 +0100

    change 'dk' to 'da' to conform to js specification

--- assets/calendar.js
+++ assets/calendar.js
@@ -4,9 +4,9 @@ require('fullcalendar/dist/gcal.min');
 
 require('fullcalendar/dist/locale/ar');
 require('fullcalendar/dist/locale/cs');
+require('fullcalendar/dist/locale/da');
 require('fullcalendar/dist/locale/de');
 require('fullcalendar/dist/locale/de-ch');
-require('fullcalendar/dist/locale/dk');
 require('fullcalendar/dist/locale/es');
 require('fullcalendar/dist/locale/fr');
 require('fullcalendar/dist/locale/hu');
