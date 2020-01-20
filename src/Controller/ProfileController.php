af4f9469db71b4f6a9dbcb778b913bb7654f0ae3
Kevin Papst <kpapst@gmx.net>
2020-01-20 01:41:45 +0100

    refactored user voter

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -281,23 +281,23 @@ class ProfileController extends AbstractController
     ): Response {
         $forms = [];
 
-        if ($this->isGranted(UserVoter::EDIT, $user)) {
+        if ($this->isGranted('edit', $user)) {
             $editForm = $editForm ?: $this->createEditForm($user);
             $forms['settings'] = $editForm->createView();
         }
-        if ($this->isGranted(UserVoter::PASSWORD, $user)) {
+        if ($this->isGranted('password', $user)) {
             $pwdForm = $pwdForm ?: $this->createPasswordForm($user);
             $forms['password'] = $pwdForm->createView();
         }
-        if ($this->isGranted(UserVoter::API_TOKEN, $user)) {
+        if ($this->isGranted('api-token', $user)) {
             $apiTokenForm = $apiTokenForm ?: $this->createApiTokenForm($user);
             $forms['api-token'] = $apiTokenForm->createView();
         }
-        if ($this->isGranted(UserVoter::TEAMS, $user) && $this->teams->count([]) > 0) {
+        if ($this->isGranted('teams', $user) && $this->teams->count([]) > 0) {
             $teamsForm = $teamsForm ?: $this->createTeamsForm($user);
             $forms['teams'] = $teamsForm->createView();
         }
-        if ($this->isGranted(UserVoter::ROLES, $user)) {
+        if ($this->isGranted('roles', $user)) {
             $rolesForm = $rolesForm ?: $this->createRolesForm($user);
             $forms['roles'] = $rolesForm->createView();
         }
