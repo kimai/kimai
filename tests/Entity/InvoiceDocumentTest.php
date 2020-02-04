db000e5cc95e2fcecbaacff12bb2214fc327602d
Kevin Papst <kpapst@gmx.net>
2020-02-04 18:05:12 +0100

    added test

---
+++ tests/Entity/InvoiceDocumentTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Entity;
+
+use App\Entity\InvoiceDocument;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \App\Entity\InvoiceDocument
+ */
+class InvoiceTemplateTest extends TestCase
+{
+    public function testDefaultValues()
+    {
+        $dir = realpath(__DIR__ . '/../../templates/invoice/renderer');
+        $sut = new InvoiceDocument(new \SplFileInfo($dir . '/default.html.twig'));
+
+        self::assertEquals('twig', $sut->getFileExtension());
+        self::assertStringContainsString('templates/invoice/renderer/default.html.twig', $sut->getFilename());
+        self::assertEquals('default', $sut->getId());
+        self::assertEquals('default.html.twig', $sut->getName());
+    }
+}
