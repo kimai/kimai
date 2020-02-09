7787ab591270daa31b47a2ed5df7c4f72ca42194
Kevin Papst <kpapst@gmx.net>
2020-02-09 18:38:38 +0100

    remove test

--- src/API/ActivityController.php
+++ src/API/ActivityController.php
@@ -12,7 +12,6 @@ declare(strict_types=1);
 namespace App\API;
 
 use App\Entity\Activity;
-use App\Entity\Project;
 use App\Event\ActivityMetaDefinitionEvent;
 use App\Form\API\ActivityApiEditForm;
 use App\Repository\ActivityRepository;
