42777988421ed6eaff0002c0abcb16b1930737fa
Kevin Papst <kpapst@gmx.net>
2020-02-16 03:45:45 +0100

    allow to use entity types in system configuration

--- src/Repository/ConfigurationRepository.php
+++ src/Repository/ConfigurationRepository.php
@@ -83,6 +83,11 @@ class ConfigurationRepository extends EntityRepository implements ConfigLoaderIn
                     $entity->setName($configuration->getName());
                 }
 
+                // this should be improved din the future
+                if (is_object($value) && method_exists($value, 'getId')) {
+                    $value = $value->getId();
+                }
+
                 $entity->setValue($value);
 
                 $em->persist($entity);
