9ccb134bd102897ac3715ff018da226538a0cef3
Honza Kopecký <honza.kopecky95@gmail.com>
2020-04-16 18:45:42 +0200

    Add configuration fields for timesheet lockdown

--- src/Configuration/TimesheetConfiguration.php
+++ src/Configuration/TimesheetConfiguration.php
@@ -72,4 +72,50 @@ class TimesheetConfiguration implements SystemBundleConfiguration
     {
         return (int) $this->find('rounding.default.duration');
     }
+
+    public function getLockdownPeriodStart(): ?\DateTime
+    {
+        try {
+            $str = (string) $this->find('rules.lockdown_period_start');
+            if ($str == null || trim($str) == '') {
+                return null;
+            }
+
+            return new \DateTime($str);
+        } catch (\Exception $ex) {
+            return null;
+        }
+    }
+
+    public function getLockdownPeriodEnd(): ?\DateTime
+    {
+        try {
+            $str = (string) $this->find('rules.lockdown_period_end');
+            if ($str == null || trim($str) == '') {
+                return null;
+            }
+
+            return new \DateTime($str);
+        } catch (\Exception $ex) {
+            return null;
+        }
+    }
+
+    public function getLockdownGracePeriod(): ?\DateTime
+    {
+        $lockdownEnd = $this->getLockdownPeriodEnd();
+        if ($lockdownEnd == null) {
+            return null;
+        }
+        try {
+            $str = (string) $this->find('rules.lockdown_grace_period');
+            if ($str == null || trim($str) == '') {
+                return null;
+            }
+
+            return new \DateTime($str . $lockdownEnd->format(' Y-m-d'));
+        } catch (\Exception $ex) {
+            return null;
+        }
+    }
 }
