c61e5c61f3ab6f8b934f2f938b5791e9ef5356b2
Alfred Berger <a.berger@avintis.com>
2020-06-28 17:39:19 +0200

    xlsx export render don't export decimal duration of timesheet if user preferences 'Use decimal duration in export' is checked #1795

--- src/Export/Base/AbstractSpreadsheetRenderer.php
+++ src/Export/Base/AbstractSpreadsheetRenderer.php
@@ -40,6 +40,7 @@ abstract class AbstractSpreadsheetRenderer
 {
     public const DATETIME_FORMAT = 'yyyy-mm-dd hh:mm';
     public const TIME_FORMAT = 'hh:mm';
+    public const DURATION_FORMAT_DEC = '#0.00';
     public const DURATION_FORMAT = '[hh]:mm';
     public const RATE_FORMAT_DEFAULT = '#.##0,00 [$%1$s];-#.##0,00 [$%1$s]';
     public const RATE_FORMAT_LEFT = '_("%1$s"* #,##0.00_);_("%1$s"* \(#,##0.00\);_("%1$s"* "-"??_);_(@_)';
@@ -61,6 +62,10 @@ abstract class AbstractSpreadsheetRenderer
      * @var AuthorizationCheckerInterface
      */
     protected $voter;
+    /**
+     * @var bool
+     */
+    protected $durationFormatDec;
     /**
      * @var array
      */
@@ -147,7 +152,12 @@ abstract class AbstractSpreadsheetRenderer
     {
         $sheet->setCellValueByColumnAndRow($column, $row, sprintf('=SUM(%s:%s)', $startCoordinate, $endCoordinate));
         $style = $sheet->getStyleByColumnAndRow($column, $row);
-        $style->getNumberFormat()->setFormatCode(self::DURATION_FORMAT);
+
+        if ($this->getDurationFormatDec()) {
+            $style->getNumberFormat()->setFormatCode(self::DURATION_FORMAT_DEC);
+        } else {
+            $style->getNumberFormat()->setFormatCode(self::DURATION_FORMAT);
+        }
     }
 
     protected function setDuration(Worksheet $sheet, $column, $row, $duration)
@@ -155,8 +165,14 @@ abstract class AbstractSpreadsheetRenderer
         if (null === $duration) {
             $duration = 0;
         }
-        $sheet->setCellValueByColumnAndRow($column, $row, sprintf('=%s/86400', $duration));
-        $sheet->getStyleByColumnAndRow($column, $row)->getNumberFormat()->setFormatCode(self::DURATION_FORMAT);
+
+        if ($this->getDurationFormatDec()) {
+            $sheet->setCellValueByColumnAndRow($column, $row, sprintf('=%s/3600', $duration));
+            $sheet->getStyleByColumnAndRow($column, $row)->getNumberFormat()->setFormatCode(self::DURATION_FORMAT_DEC);
+        } else {
+            $sheet->setCellValueByColumnAndRow($column, $row, sprintf('=%s/86400', $duration));
+            $sheet->getStyleByColumnAndRow($column, $row)->getNumberFormat()->setFormatCode(self::DURATION_FORMAT);
+        }
     }
 
     protected function setRateTotal(Worksheet $sheet, $column, $row, $startCoordinate, $endCoordinate)
@@ -191,6 +207,13 @@ abstract class AbstractSpreadsheetRenderer
      */
     protected function getColumns(array $exportItems, TimesheetQuery $query, array $columns): array
     {
+        $decFormat = true;
+        if (null !== $query->getCurrentUser()) {
+            $this->setDurationFormatDec((bool) $query->getCurrentUser()->getPreferenceValue('timesheet.export_decimal', $decFormat));
+        } elseif (null !== $query->getUser()) {
+            $this->setDurationFormatDec((bool) $query->getUser()->getPreferenceValue('timesheet.export_decimal', $decFormat));
+        }
+
         $showRates = $this->isRenderRate($query);
 
         if (isset($columns['date']) && !isset($columns['date']['render'])) {
@@ -632,4 +655,28 @@ abstract class AbstractSpreadsheetRenderer
      * @throws \Exception
      */
     abstract protected function saveSpreadsheet(Spreadsheet $spreadsheet): string;
+
+    /**
+     * Get the value of durationFormatDec
+     *
+     * @return bool
+     */
+    public function getDurationFormatDec(): bool
+    {
+        return $this->durationFormatDec;
+    }
+
+    /**
+     * Set the value of durationFormatDec
+     *
+     * @param bool $durationFormatDec
+     *
+     * @return self
+     */
+    public function setDurationFormatDec(bool $durationFormatDec)
+    {
+        $this->durationFormatDec = $durationFormatDec;
+
+        return $this;
+    }
 }
