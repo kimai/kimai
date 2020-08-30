e4a62b9b94d0e7cd761eb2625bce604dbd1454d0
Kevin Papst <kpapst@gmx.net>
2020-08-30 12:41:35 +0200

    cleanup global context

--- src/API/BaseApiController.php
+++ src/API/BaseApiController.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 namespace App\API;
 
 use App\Entity\User;
+use App\Timesheet\DateTimeFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
@@ -22,4 +23,13 @@ abstract class BaseApiController extends AbstractController
 {
     public const DATE_FORMAT = DateTimeType::HTML5_FORMAT;
     public const DATE_FORMAT_PHP = 'Y-m-d\TH:i:s';
+
+    protected function getDateTimeFactory(?User $user = null): DateTimeFactory
+    {
+        if (null === $user) {
+            $user = $this->getUser();
+        }
+
+        return new DateTimeFactory(new \DateTimeZone($user->getTimezone()));
+    }
 }
