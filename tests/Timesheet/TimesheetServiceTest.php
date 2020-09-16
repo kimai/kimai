3d4b3fbc265394ad8eef616b527097a21e4f3647
Kevin Papst <kpapst@gmx.net>
2020-09-16 20:15:55 +0200

    more tests

--- tests/Timesheet/TimesheetServiceTest.php
+++ tests/Timesheet/TimesheetServiceTest.php
@@ -13,11 +13,14 @@ use App\Configuration\TimesheetConfiguration;
 use App\Entity\Timesheet;
 use App\Event\TimesheetCreatePostEvent;
 use App\Event\TimesheetCreatePreEvent;
+use App\Event\TimesheetDeleteMultiplePreEvent;
+use App\Event\TimesheetDeletePreEvent;
 use App\Event\TimesheetRestartPostEvent;
 use App\Event\TimesheetRestartPreEvent;
 use App\Repository\TimesheetRepository;
 use App\Timesheet\TimesheetService;
 use App\Timesheet\TrackingModeService;
+use App\Validator\ValidationException;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
@@ -124,5 +127,63 @@ class TimesheetServiceTest extends TestCase
         $sut = $this->getSut($authorizationChecker, $dispatcher);
 
         $sut->restartTimesheet($timesheet, new Timesheet());
+    }
+
+    public function testPreparePersistedTimesheetAsNew()
+    {
+        $timesheet = $this->createMock(Timesheet::class);
+        $timesheet->expects($this->once())->method('getId')->willReturn(1);
+
+        $sut = $this->getSut();
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Cannot prepare timesheet, already persisted');
+
+        $sut->prepareNewTimesheet($timesheet);
+    }
+
+    public function testStoppedEntriesCannotBeStoppedAgain()
+    {
+        $timesheet = new Timesheet();
+        $timesheet->setEnd(new \DateTime());
+
+        $sut = $this->getSut();
+
+        $this->expectException(ValidationException::class);
+        $this->expectExceptionMessage('Timesheet entry already stopped');
+
+        $sut->stopTimesheet($timesheet);
+    }
+
+    public function testDeleteDispatchesEvent()
+    {
+        $timesheet = new Timesheet();
+
+        $dispatcher = $this->createMock(EventDispatcherInterface::class);
+        $dispatcher->method('dispatch')->willReturnCallback(function ($event) use ($timesheet) {
+            self::assertInstanceOf(TimesheetDeletePreEvent::class, $event);
+            /* @var TimesheetDeletePreEvent $event */
+            self::assertSame($timesheet, $event->getTimesheet());
+        });
+
+        $sut = $this->getSut(null, $dispatcher);
+
+        $sut->deleteTimesheet($timesheet);
+    }
+
+    public function testDeleteMultipleDispatchesEvent()
+    {
+        $timesheets = [new Timesheet(), new Timesheet()];
+
+        $dispatcher = $this->createMock(EventDispatcherInterface::class);
+        $dispatcher->method('dispatch')->willReturnCallback(function ($event) use ($timesheets) {
+            self::assertInstanceOf(TimesheetDeleteMultiplePreEvent::class, $event);
+            /* @var TimesheetDeleteMultiplePreEvent $event */
+            self::assertSame($timesheets, $event->getTimesheets());
+        });
+
+        $sut = $this->getSut(null, $dispatcher);
+
+        $sut->deleteMultipleTimesheets($timesheets);
     }
 }
