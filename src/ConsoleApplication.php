ab7c1c5093d8b14b1442ca1faf768d6eeab8f96b
Kevin Papst <kevinpapst@users.noreply.github.com>
2020-11-29 20:03:06 +0100

    display application name and version in console (#2150)

---
+++ src/ConsoleApplication.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+
+class ConsoleApplication extends Application
+{
+    public function getName()
+    {
+        return Constants::SOFTWARE;
+    }
+
+    public function getVersion()
+    {
+        return Constants::VERSION;
+    }
+}
