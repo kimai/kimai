6756ab2377fab998ef6d74c953932d8100153a0d
Kevin Papst <kpapst@gmx.net>
2021-03-07 15:48:13 +0100

    remove sqlite support from migrations

--- src/Migrations/Version20191024100951.php
+++ src/Migrations/Version20191024100951.php
@@ -26,21 +26,6 @@ final class Version20191024100951 extends AbstractMigration
         return 'Adds the order_date column to the projects table';
     }
 
-    protected function isSupportingForeignKeys(): bool
-    {
-        return false;
-    }
-
-    public function isTransactional(): bool
-    {
-        if ($this->isPlatformSqlite()) {
-            // does fail if we use transactions, as tables are re-created and foreign keys would fail
-            return false;
-        }
-
-        return true;
-    }
-
     public function up(Schema $schema): void
     {
         $projects = $schema->getTable('kimai2_projects');
--- src/Migrations/Version20191108151534.php
+++ src/Migrations/Version20191108151534.php
@@ -26,21 +26,6 @@ final class Version20191108151534 extends AbstractMigration
         return 'Adds the user roles and role permissions table';
     }
 
-    protected function isSupportingForeignKeys(): bool
-    {
-        return false;
-    }
-
-    public function isTransactional(): bool
-    {
-        if ($this->isPlatformSqlite()) {
-            // does fail if we use transactions, as tables are re-created and foreign keys would fail
-            return false;
-        }
-
-        return true;
-    }
-
     public function up(Schema $schema): void
     {
         $roles = $schema->createTable('kimai2_roles');
