5eda44790c9ab8be0bb8e2b985bc563c110527aa
Kevin Papst <kpapst@gmx.net>
2021-03-08 14:07:48 +0100

    allow duration via api

--- src/API/TimesheetController.php
+++ src/API/TimesheetController.php
@@ -340,6 +340,7 @@ class TimesheetController extends BaseApiController
             'allow_begin_datetime' => $mode->canUpdateTimesWithAPI(),
             'allow_end_datetime' => $mode->canUpdateTimesWithAPI(),
             'date_format' => self::DATE_FORMAT,
+            'allow_duration' => $mode->canUpdateTimesWithAPI(),
         ]);
 
         $form->submit($request->request->all(), false);
@@ -424,6 +425,7 @@ class TimesheetController extends BaseApiController
             'allow_begin_datetime' => $mode->canUpdateTimesWithAPI(),
             'allow_end_datetime' => $mode->canUpdateTimesWithAPI(),
             'date_format' => self::DATE_FORMAT,
+            'allow_duration' => $mode->canUpdateTimesWithAPI(),
         ]);
 
         $form->setData($timesheet);
