e52a333674618d801841e067cbff753a582a056c
Kevin Papst <kpapst@gmx.net>
2021-03-11 16:14:40 +0100

    moved actions to event subscriber

---
+++ src/EventSubscriber/Actions/TeamSubscriber.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\EventSubscriber\Actions;
+
+use App\Entity\Team;
+use App\Event\PageActionsEvent;
+
+class TeamSubscriber extends AbstractActionsSubscriber
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'actions.team' => ['onActions', 1000],
+        ];
+    }
+
+    public function onActions(PageActionsEvent $event)
+    {
+        $payload = $event->getPayload();
+
+        if (!\is_array($payload) || !\array_key_exists('team', $payload)) {
+            return;
+        }
+
+        /** @var Team $team */
+        $team = $payload['team'];
+
+        if (!$event->isIndexView() && $this->isGranted('view_tag')) {
+            //$event->addBack($this->path('admin_team'));
+        }
+
+        if ($team->getId() === null) {
+            return;
+        }
+
+        if ($this->isGranted('edit', $team)) {
+            $event->addAction('edit', ['url' => $this->path('admin_team_edit', ['id' => $team->getId()])]);
+
+            if ($this->isGranted('create_team')) {
+                $event->addAction('copy', ['url' => $this->path('team_duplicate', ['id' => $team->getId()])]);
+            }
+        }
+
+        if ($event->isIndexView() && $this->isGranted('delete', $team)) {
+            $event->addAction('trash', [
+                'url' => $this->path('delete_team', ['id' => $team->getId()]),
+                'class' => 'api-link',
+                'attr' => [
+                    'data-event' => 'kimai.teamDelete kimai.teamUpdate',
+                    'data-method' => 'DELETE',
+                    'data-question' => 'confirm.delete',
+                    'data-msg-error' => 'action.delete.error',
+                    'data-msg-success' => 'action.delete.success'
+                ]
+            ]);
+        }
+    }
+}
