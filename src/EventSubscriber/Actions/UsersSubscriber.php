d72d627903e3d82da6d7279aa26c5f206a1a105a
Kevin Papst <kpapst@gmx.net>
2021-03-11 14:12:02 +0100

    moved actions to event subscriber

---
+++ src/EventSubscriber/Actions/UsersSubscriber.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\EventSubscriber\Actions;
+
+use App\Event\PageActionsEvent;
+
+class UsersSubscriber extends AbstractActionsSubscriber
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'actions.users' => ['onActions', 1000],
+        ];
+    }
+
+    public function onActions(PageActionsEvent $event)
+    {
+        $event->addSearchToggle();
+        if ($event->isIndexView()) {
+            $event->addColumnToggle('#modal_user_admin');
+        }
+        $event->addQuickExport($this->path('user_export'));
+
+        if ($this->isGranted('create_user')) {
+            $event->addCreate($this->path('admin_user_create'));
+        }
+
+        $event->addHelp($this->documentationLink('users.html'));
+    }
+}
