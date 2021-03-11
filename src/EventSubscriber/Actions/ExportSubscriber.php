8fe7ae2cdf0cd8709e7cb3e69c6586c46372a2ae
Kevin Papst <kpapst@gmx.net>
2021-03-11 15:58:14 +0100

    moved actions to event subscriber

---
+++ src/EventSubscriber/Actions/ExportSubscriber.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\EventSubscriber\Actions;
+
+use App\Event\PageActionsEvent;
+
+class ExportSubscriber extends AbstractActionsSubscriber
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'actions.export' => ['onActions', 1000],
+        ];
+    }
+
+    public function onActions(PageActionsEvent $event)
+    {
+        $event->addColumnToggle('#modal_export');
+
+        if ($event->isView('preview')) {
+            $event->addAction('off', ['id' => 'export-toggle-button']);
+        }
+
+        $event->addHelp($this->documentationLink('export.html'));
+    }
+}
