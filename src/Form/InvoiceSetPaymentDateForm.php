75de8d982b663811df16fcc8acb3426230ba9f3f
Philipp Müller <pmueller.wf@gmail.com>
2021-03-21 13:28:46 +0100

    composer codestyle-fix

--- src/Form/InvoiceSetPaymentDateForm.php
+++ src/Form/InvoiceSetPaymentDateForm.php
@@ -11,14 +11,8 @@ namespace App\Form;
 
 use App\Entity\Customer;
 use App\Entity\Invoice;
-use App\Entity\Project;
-use App\Form\Type\CustomerType;
 use App\Form\Type\DateTimePickerType;
-use App\Repository\CustomerRepository;
-use App\Repository\Query\CustomerFormTypeQuery;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
--- src/Migrations/Version20210320162820.php
+++ src/Migrations/Version20210320162820.php
@@ -2,6 +2,13 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
