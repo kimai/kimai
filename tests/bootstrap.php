04b8823d934ead958590e22c55b1b01ba8575f1c
Toby Batch <tobias@neontribe.co.uk>
2021-06-23 08:33:41 +0100

    chore: reinstated phpunit db tearup

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,13 +12,13 @@ require __DIR__ . '/../vendor/autoload.php';
 if (isset($_ENV['BOOTSTRAP_RESET_DATABASE']) && $_ENV['BOOTSTRAP_RESET_DATABASE'] == true) {
     echo 'Re-Installing test database ...' . PHP_EOL;
 
-//    exec(sprintf(
-//        'APP_ENV=test php "%s/../bin/console" kimai:reset-test --env=test --no-interaction -vvv',
-//        __DIR__
-//    ), $output, $exitCode);
+    exec(sprintf(
+        'APP_ENV=test php "%s/../bin/console" kimai:reset-test --env=test --no-interaction -vvv',
+        __DIR__
+    ), $output, $exitCode);
 
-//    if ($exitCode !== 0) {
-//        dump($output);
-//        throw new \Exception('Failed to setup test database.');
-//    }
+    if ($exitCode !== 0) {
+        dump($output);
+        throw new \Exception('Failed to setup test database.');
+    }
 }
