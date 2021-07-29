974907468d0aba30a24066bf76926d8fcf637149
Kevin Papst <kpapst@gmx.net>
2021-07-29 23:06:16 +0200

    stop button first in action menu

--- src/EventSubscriber/Actions/AbstractTimesheetSubscriber.php
+++ src/EventSubscriber/Actions/AbstractTimesheetSubscriber.php
@@ -24,17 +24,17 @@ abstract class AbstractTimesheetSubscriber extends AbstractActionsSubscriber
         /** @var Timesheet $timesheet */
         $timesheet = $payload['timesheet'];
         if ($timesheet->getId() !== null) {
-            if ($this->isGranted('edit', $timesheet)) {
-                $class = $event->isView('edit') ? '' : 'modal-ajax-form';
-                $event->addAction('edit', ['url' => $this->path($routeEdit, ['id' => $timesheet->getId()]), 'class' => $class]);
-            }
-
             if ($timesheet->isRunning() && $this->isGranted('stop', $timesheet)) {
                 $event->addAction('stop', ['url' => $this->path('stop_timesheet', ['id' => $timesheet->getId()]), 'class' => 'api-link', 'attr' => ['data-event' => 'kimai.timesheetStop kimai.timesheetUpdate', 'data-method' => 'PATCH', 'data-msg-error' => 'timesheet.stop.error', 'data-msg-success' => 'timesheet.stop.success']]);
             }
 
             if (!$timesheet->isRunning() && $this->isGranted('start', $timesheet)) {
                 $event->addAction('repeat', ['url' => $this->path('restart_timesheet', ['id' => $timesheet->getId()]), 'class' => 'api-link', 'attr' => ['data-payload' => '{"copy": "all"}', 'data-event' => 'kimai.timesheetStart kimai.timesheetUpdate', 'data-method' => 'PATCH', 'data-msg-error' => 'timesheet.start.error', 'data-msg-success' => 'timesheet.start.success']]);
+            }
+
+            if ($this->isGranted('edit', $timesheet)) {
+                $class = $event->isView('edit') ? '' : 'modal-ajax-form';
+                $event->addAction('edit', ['url' => $this->path($routeEdit, ['id' => $timesheet->getId()]), 'class' => $class]);
             }
 
             if ($this->isGranted('duplicate', $timesheet)) {
