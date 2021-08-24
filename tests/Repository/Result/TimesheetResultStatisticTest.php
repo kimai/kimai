d765dc17858962146eae81bf7aa7b621921e1cdb
Kevin Papst <kpapst@gmx.net>
2021-08-24 18:33:53 +0200

    added test

---
+++ tests/Repository/Result/TimesheetResultStatisticTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Repository\Result;
+
+use App\Repository\Result\TimesheetResultStatistic;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \App\Repository\Result\TimesheetResultStatistic
+ */
+class TimesheetResultStatisticTest extends TestCase
+{
+    public function testConstruct()
+    {
+        $sut = new TimesheetResultStatistic(13, 7705);
+        self::assertSame(13, $sut->getCount());
+        self::assertSame(7705, $sut->getDuration());
+    }
+}
