8eb5f35bee48a81d19dec8b6d6268e096d0bd011
Kevin Papst <kpapst@gmx.net>
2021-08-25 13:53:30 +0200

    prevent email or username becoming non-unique

--- src/Validator/Constraints/UserValidator.php
+++ src/Validator/Constraints/UserValidator.php
@@ -44,28 +44,41 @@ class UserValidator extends ConstraintValidator
 
     protected function validateUser(UserEntity $user, ExecutionContextInterface $context)
     {
+        $matchedEmail = false;
         if ($user->getEmail() !== null) {
-            $existingByEmail = $this->userService->findUserByEmail($user->getEmail());
-
-            if (null !== $existingByEmail && $user->getId() !== $existingByEmail->getId()) {
-                $context->buildViolation(User::getErrorName(User::USER_EXISTING_EMAIL))
-                    ->atPath('email')
-                    ->setTranslationDomain('validators')
-                    ->setCode(User::USER_EXISTING_EMAIL)
-                    ->addViolation();
-            }
+            $this->validateEmailExists($user->getId(), $user->getEmail(), 'email', User::USER_EXISTING_EMAIL, $context);
+            $this->validateEmailExists($user->getId(), $user->getUsername(), 'username', User::USER_EXISTING_NAME_AS_EMAIL, $context);
         }
 
         if ($user->getUsername() !== null) {
-            $existingByName = $this->userService->findUserByName($user->getUsername());
+            $this->validateUsernameExists($user->getId(), $user->getUsername(), 'username', User::USER_EXISTING_NAME, $context);
+            $this->validateUsernameExists($user->getId(), $user->getEmail(), 'email', User::USER_EXISTING_EMAIL_AS_NAME, $context);
+        }
+    }
 
-            if (null !== $existingByName && $user->getId() !== $existingByName->getId()) {
-                $context->buildViolation(User::getErrorName(User::USER_EXISTING_NAME))
-                    ->atPath('username')
-                    ->setTranslationDomain('validators')
-                    ->setCode(User::USER_EXISTING_NAME)
-                    ->addViolation();
-            }
+    private function validateEmailExists(?int $userId, string $email, string $path, string $code, ExecutionContextInterface $context): void
+    {
+        $existingByEmail = $this->userService->findUserByEmail($email);
+
+        if (null !== $existingByEmail && $userId !== $existingByEmail->getId()) {
+            $context->buildViolation(User::getErrorName($code))
+                ->atPath($path)
+                ->setTranslationDomain('validators')
+                ->setCode($code)
+                ->addViolation();
+        }
+    }
+
+    private function validateUsernameExists(?int $userId, string $username, string $path, string $code, ExecutionContextInterface $context): void
+    {
+        $existingByName = $this->userService->findUserByName($username);
+
+        if (null !== $existingByName && $userId !== $existingByName->getId()) {
+            $context->buildViolation(User::getErrorName($code))
+                ->atPath($path)
+                ->setTranslationDomain('validators')
+                ->setCode($code)
+                ->addViolation();
         }
     }
 }
