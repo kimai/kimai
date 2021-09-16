5a56eab4fd7fc3b1182025bcdd0e9bdad6f308e3
Kevin Papst <kpapst@gmx.net>
2021-09-16 14:11:39 +0200

    removed customer field from activity form

--- src/Form/ActivityEditForm.php
+++ src/Form/ActivityEditForm.php
@@ -11,18 +11,13 @@ namespace App\Form;
 
 use App\Entity\Activity;
 use App\Entity\Customer;
-use App\Form\Type\CustomerType;
 use App\Form\Type\ProjectType;
-use App\Repository\CustomerRepository;
 use App\Repository\ProjectRepository;
-use App\Repository\Query\CustomerFormTypeQuery;
 use App\Repository\Query\ProjectFormTypeQuery;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ActivityEditForm extends AbstractType
@@ -67,24 +62,6 @@ class ActivityEditForm extends AbstractType
             ])
         ;
 
-        if ($new) {
-            if ($options['customer']) {
-                $builder
-                    ->add('customer', CustomerType::class, [
-                        'query_builder' => function (CustomerRepository $repo) use ($builder, $customer) {
-                            $query = new CustomerFormTypeQuery($customer);
-                            $query->setUser($builder->getOption('user'));
-
-                            return $repo->getQueryBuilderForFormType($query);
-                        },
-                        'data' => $customer ? $customer : null,
-                        'required' => false,
-                        'mapped' => false,
-                        'project_enabled' => true,
-                    ]);
-            }
-        }
-
         if ($new || !$isGlobal) {
             $builder
                 ->add('project', ProjectType::class, [
@@ -97,28 +74,6 @@ class ActivityEditForm extends AbstractType
                         return $repo->getQueryBuilderForFormType($query);
                     },
                 ]);
-
-            // replaces the project select after submission, to make sure only projects for the selected customer are displayed
-            $builder->addEventListener(
-                FormEvents::PRE_SUBMIT,
-                function (FormEvent $event) use ($builder, $project) {
-                    $data = $event->getData();
-                    if (!isset($data['customer']) || empty($data['customer'])) {
-                        return;
-                    }
-
-                    $event->getForm()->add('project', ProjectType::class, [
-                        'group_by' => null,
-                        'query_builder' => function (ProjectRepository $repo) use ($builder, $data, $project) {
-                            $query = new ProjectFormTypeQuery($project, $data['customer']);
-                            $query->setUser($builder->getOption('user'));
-                            $query->setIgnoreDate(true);
-
-                            return $repo->getQueryBuilderForFormType($query);
-                        },
-                    ]);
-                }
-            );
         }
 
         $this->addCommonFields($builder, $options);
@@ -134,6 +89,7 @@ class ActivityEditForm extends AbstractType
             'csrf_protection' => true,
             'csrf_field_name' => '_token',
             'csrf_token_id' => 'admin_activity_edit',
+            // @deprecated not supported since 1.15, which removed the customer select completely
             'customer' => false,
             'currency' => Customer::DEFAULT_CURRENCY,
             'include_budget' => false,
