17b021655f386b4aa7f1402205674879491adc26
Kevin Papst <kpapst@gmx.net>
2022-03-28 00:48:57 +0200

    refactor to use new AuthenticationUtils

--- src/Controller/Security/SecurityController.php
+++ src/Controller/Security/SecurityController.php
@@ -10,52 +10,24 @@
 namespace App\Controller\Security;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 final class SecurityController extends AbstractController
 {
-    private $tokenManager;
-
-    public function __construct(CsrfTokenManagerInterface $tokenManager)
+    public function __construct(private CsrfTokenManagerInterface $tokenManager)
     {
-        $this->tokenManager = $tokenManager;
     }
 
     /**
      * @Route(path="/login", name="login", methods={"GET", "POST"})
      */
-    public function loginAction(Request $request): Response
+    public function loginAction(AuthenticationUtils $authenticationUtils): Response
     {
-        /** @var SessionInterface $session */
-        $session = $request->getSession();
-
-        $authErrorKey = Security::AUTHENTICATION_ERROR;
-        $lastUsernameKey = Security::LAST_USERNAME;
-
-        // get the error if any (works with forward and redirect -- see below)
-        if ($request->attributes->has($authErrorKey)) {
-            $error = $request->attributes->get($authErrorKey);
-        } elseif (null !== $session && $session->has($authErrorKey)) {
-            $error = $session->get($authErrorKey);
-            $session->remove($authErrorKey);
-        } else {
-            $error = null;
-        }
-
-        if (!$error instanceof AuthenticationException) {
-            $error = null; // The value does not come from the security component.
-        }
-
-        $lastUsername = '';
-        if ($request->hasSession()) {
-            $lastUsername = $session->get($lastUsernameKey);
-        }
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
 
         $csrfToken = $this->tokenManager->getToken('authenticate')->getValue();
 
