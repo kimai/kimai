100f8a5165c7d455b2a4c79a10a2abf185aaa394
Kevin Papst <kevinpapst@users.noreply.github.com>
2022-04-29 18:14:12 +0200

    Release 1.19.6 (#3277)

--- src/Constants.php
+++ src/Constants.php
@@ -17,11 +17,11 @@ class Constants
     /**
      * The current release version
      */
-    public const VERSION = '1.19.5';
+    public const VERSION = '1.19.6';
     /**
      * The current release: major * 10000 + minor * 100 + patch
      */
-    public const VERSION_ID = 11905;
+    public const VERSION_ID = 11906;
     /**
      * The current release status, either "stable" or "dev"
      */
