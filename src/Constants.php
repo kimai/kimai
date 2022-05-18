3e6100f368cfcd7d25dc3a7c791b860e0751df2a
Kevin Papst <kevinpapst@users.noreply.github.com>
2022-05-19 01:36:12 +0200

    bump version to 1.20 (#3315)

--- src/Constants.php
+++ src/Constants.php
@@ -17,11 +17,11 @@ class Constants
     /**
      * The current release version
      */
-    public const VERSION = '1.19.8';
+    public const VERSION = '1.20';
     /**
      * The current release: major * 10000 + minor * 100 + patch
      */
-    public const VERSION_ID = 11908;
+    public const VERSION_ID = 12000;
     /**
      * The current release status, either "stable" or "dev"
      */
