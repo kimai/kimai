cc0f334de6d8da8bc7886d96f3468ecff590ed3f
Kevin Papst <kpapst@gmx.net>
2022-06-18 19:57:30 +0200

    code style

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -18,7 +18,6 @@ use App\Repository\Query\TeamQuery;
 use App\Repository\TeamRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\Form\FormInterface;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
