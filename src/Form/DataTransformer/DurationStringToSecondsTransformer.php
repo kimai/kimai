5626557e2abf60bd6dd3b8d77cf24dc7b71ab1db
Kevin Papst <kpapst@gmx.net>
2022-07-03 12:12:35 +0200

    remove constructor work

--- src/Form/DataTransformer/DurationStringToSecondsTransformer.php
+++ src/Form/DataTransformer/DurationStringToSecondsTransformer.php
@@ -17,29 +17,13 @@ use Symfony\Component\Form\Exception\TransformationFailedException;
 class DurationStringToSecondsTransformer implements DataTransformerInterface
 {
     /**
-     * @var Duration
-     */
-    protected $formatter;
-    /**
-     * @var string
-     */
-    private $pattern;
-
-    public function __construct()
-    {
-        $this->formatter = new Duration();
-        $constraint = new DurationConstraint();
-        $this->pattern = $constraint->pattern;
-    }
-
-    /**
      * @param int $intToFormat
      * @return string|null
      */
-    public function transform(mixed $intToFormat): mixed
+    public function transform(mixed $intToFormat): ?string
     {
         try {
-            return $this->formatter->format($intToFormat);
+            return (new Duration())->format($intToFormat);
         } catch (\Exception | \TypeError $e) {
             throw new TransformationFailedException($e->getMessage());
         }
@@ -49,7 +33,7 @@ class DurationStringToSecondsTransformer implements DataTransformerInterface
      * @param string|null $formatToInt
      * @return int|null
      */
-    public function reverseTransform(mixed $formatToInt): mixed
+    public function reverseTransform(mixed $formatToInt): ?int
     {
         if (null === $formatToInt) {
             return null;
@@ -60,12 +44,12 @@ class DurationStringToSecondsTransformer implements DataTransformerInterface
         }
 
         // we need this one here, because the data transformer is executed BEFORE the constraint is called
-        if (!preg_match($this->pattern, $formatToInt)) {
+        if (!preg_match((new DurationConstraint())->pattern, $formatToInt)) {
             throw new TransformationFailedException('Invalid duration format given');
         }
 
         try {
-            $seconds = $this->formatter->parseDurationString($formatToInt);
+            $seconds = (new Duration())->parseDurationString($formatToInt);
 
             // DateTime throws if a duration with too many seconds is passed and an amount of so
             // many seconds is likely not required in a time-tracking application ;-)
