ba2585d62af20f4b7cfa60d84457e0ca9037f3fd
Kevin Papst <kpapst@gmx.net>
2022-07-05 20:43:31 +0200

    refactored all JS that targets forms

--- assets/js/plugins/KimaiForm.js
+++ assets/js/plugins/KimaiForm.js
@@ -10,6 +10,7 @@
  */
 
 import KimaiPlugin from "../KimaiPlugin";
+import KimaiFormPlugin from "../forms/KimaiFormPlugin";
 
 export default class KimaiForm extends KimaiPlugin {
 
@@ -20,90 +21,33 @@ export default class KimaiForm extends KimaiPlugin {
 
     activateForm(formSelector)
     {
-        const form = document.querySelector(formSelector);
-        if (form === null) {
+        const forms = document.querySelectorAll(formSelector);
+        if (forms.length === 0) {
             return;
         }
 
-        // do not init in init(), but just in case there is a page which does not have a form
-        if (this.dateTimeWidgetHandler === undefined) {
-            this.dateTimeWidgetHandler = (event) => {
-                let linkTarget = event.target;
-
-                // the HTML structure is <a href="" data-format="HH:MM" data-target="formElementId"><i class="icon"></i></a>
-                if (linkTarget.tagName.toUpperCase() === 'I') {
-                    linkTarget = linkTarget.parentElement;
+        for (const form of forms) {
+            for (const plugin of this.getContainer().getPlugins()) {
+                if (plugin instanceof KimaiFormPlugin && plugin.supportsForm(form)) {
+                    plugin.activateForm(form);
                 }
-
-                const formElement = document.getElementById(linkTarget.dataset.target);
-                if (!formElement.disabled) {
-                    formElement.value = this.getDateUtils().format(linkTarget.dataset.format, null);
-                    formElement.dispatchEvent(new Event('change', {bubbles: true}));
-                }
-
-                event.preventDefault();
-
-                return false;
-            };
-        }
-
-        [].slice.call(document.querySelectorAll(formSelector + ' a.kimai-date-widget')).map((element) => {
-            element.addEventListener('click', this.dateTimeWidgetHandler);
-        });
-
-        // used for the duration plugin, but can be used for more
-        [].slice.call(document.querySelectorAll(formSelector + ' a[data-copy-target]')).map((element) => {
-            if (element.dataset.copyTarget !== undefined) {
-                element.addEventListener('click', (event) => {
-                    const target = document.querySelector(element.dataset.copyTarget);
-                    target.value = element.dataset.copyValue;
-                    for (const event of element.dataset.copyEvent.split(' ')) {
-                        target.dispatchEvent(new Event(event));
-                    }
-                    event.preventDefault();
-                });
             }
-        });
-
-        // TODO do not register them as global plugins, but only as plugins for each form inside this class
-        this.getPlugin('date-range-picker').activate(formSelector);
-        this.getPlugin('date-picker').activate(formSelector);
-        this.getPlugin('autocomplete').activateAutocomplete(formSelector);
-        this.getPlugin('form-select').activateSelectPicker(formSelector);
-
-        switch (form.name) {
-            case 'timesheet_edit_form':
-            case 'timesheet_admin_edit_form':
-                this.getPlugin('edit-timesheet-form').activateForm(form);
-                break;
         }
     }
 
     destroyForm(formSelector)
     {
-        const form = document.querySelector(formSelector);
-        if (form === null) {
+        const forms = document.querySelectorAll(formSelector);
+        if (forms.length === 0) {
             return;
         }
 
-        if (this.dateTimeWidgetHandler !== undefined) {
-            [].slice.call(document.querySelectorAll(formSelector + ' a.kimai-date-widget')).map((element) => {
-                element.removeEventListener('click', this.dateTimeWidgetHandler);
-            });
-
-            delete this.dateTimeWidgetHandler;
-        }
-
-        this.getPlugin('form-select').destroySelectPicker(formSelector);
-        this.getPlugin('autocomplete').destroyAutocomplete(formSelector);
-        this.getPlugin('date-picker').destroy(formSelector);
-        this.getPlugin('date-range-picker').destroy(formSelector);
-
-        switch (form.name) {
-            case 'timesheet_edit_form':
-            case 'timesheet_admin_edit_form':
-                this.getPlugin('edit-timesheet-form').destroyForm(form);
-                break;
+        for (const form of forms) {
+            for (const plugin of this.getContainer().getPlugins()) {
+                if (plugin instanceof KimaiFormPlugin && plugin.supportsForm(form)) {
+                    plugin.destroyForm(form);
+                }
+            }
         }
     }
 
