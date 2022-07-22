445a96fd8b8b72b59699bd7d909aefb642ac3a96
Kevin Papst <kpapst@gmx.net>
2022-07-23 00:07:24 +0200

    fix default value

--- src/Entity/Activity.php
+++ src/Entity/Activity.php
@@ -131,7 +131,7 @@ class Activity implements EntityWithMetaFields, EntityWithBudget
      *
      * @Exporter\Expose(label="label.billable", type="boolean")
      *
-     * @ORM\Column(name="billable", type="boolean", nullable=false)
+     * @ORM\Column(name="billable", type="boolean", nullable=false, options={"default": true})
      * @Assert\NotNull()
      */
     private $billable = true;
