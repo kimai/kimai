ec99ee8b8fba555a32025a09a41e5d2c02268293
Kevin Papst <kpapst@gmx.net>
2022-07-28 20:35:06 +0200

    improved upgrade path

--- migrations/Version20993112235958.php
+++ migrations/Version20993112235958.php
@@ -15,7 +15,7 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 /**
- * @version 2.00
+ * @version 2.0
  */
 final class Version20993112235958 extends AbstractMigration
 {
@@ -53,20 +53,7 @@ final class Version20993112235958 extends AbstractMigration
             $timesheet->addIndex(['end_time', 'user'], 'IDX_4F60C6B1415614018D93D649');
         }
 
-//        return;
-
         $this->addSql("UPDATE kimai2_invoice_templates SET `language` = 'en' WHERE `language` IS NULL");
-
-        //$this->addSql('UPDATE kimai2_user_preferences SET `value` = "boxed" WHERE `name` = "theme.layout"');
-        //$this->addSql('UPDATE kimai2_user_preferences SET `value` = "default" WHERE `name` = "skin"');
-        //$this->addSql('UPDATE kimai2_user_preferences SET `value` = "1" WHERE `name` = "theme.collapsed_sidebar"');
-        //$this->addSql('UPDATE kimai2_configuration SET `value` = "default" WHERE `name` = "defaults.user.theme"');
-        //$this->addSql('DELETE FROM kimai2_configuration WHERE `name` = "theme.autocomplete_chars"');
-        //$this->addSql('DELETE FROM kimai2_configuration WHERE `name` = "theme.tags_create"');
-
-        //$this->addSql("DELETE FROM kimai2_user_preferences where `name` = 'reporting.initial_view'");
-        //$this->addSql("DELETE FROM kimai2_user_preferences where `name` = 'hours_24'");
-
         $this->addSql("UPDATE kimai2_user_preferences SET `name` = 'timesheet_daily_stats' WHERE `name` = 'timesheet.daily_stats'");
         $this->addSql("UPDATE kimai2_user_preferences SET `name` = 'collapsed_sidebar' WHERE `name` = 'theme.collapsed_sidebar'");
         $this->addSql("UPDATE kimai2_user_preferences SET `name` = 'layout' WHERE `name` = 'theme.layout'");
@@ -75,11 +62,10 @@ final class Version20993112235958 extends AbstractMigration
         $this->addSql("UPDATE kimai2_user_preferences SET `name` = 'export_decimal' WHERE `name` = 'timesheet.export_decimal'");
         $this->addSql("UPDATE kimai2_user_preferences SET `name` = 'update_browser_title' WHERE `name` = 'theme.update_browser_title'");
 
-        // TODO user configuration calendar first day agendaDay = day / agendaMonth = month
-        // TODO delete timesheet.active_entries.soft_limit
         // TODO update SET `value` = '15' kimai2_configuration WHERE `name` = 'timesheet.time_increment' and value = '0'
-
-        $this->addSql("UPDATE kimai2_configuration SET `value` = 'default' WHERE `name` = 'timesheet.mode' and `value` = 'duration_only'");
+        //$this->addSql('UPDATE kimai2_user_preferences SET `value` = "boxed" WHERE `name` = "theme.layout"');
+        //$this->addSql('UPDATE kimai2_user_preferences SET `value` = "default" WHERE `name` = "skin"');
+        //$this->addSql('UPDATE kimai2_configuration SET `value` = "default" WHERE `name` = "defaults.user.theme"');
     }
 
     public function down(Schema $schema): void
@@ -102,8 +88,6 @@ final class Version20993112235958 extends AbstractMigration
             $timesheet->dropIndex('IDX_4F60C6B1415614018D93D649');
         }
 
-//        return;
-
         $this->addSql("UPDATE kimai2_user_preferences SET `name` = 'theme.collapsed_sidebar' WHERE `name` = 'collapsed_sidebar'");
         $this->addSql("UPDATE kimai2_user_preferences SET `name` = 'theme.layout' WHERE `name` = 'theme_layout'");
         $this->addSql("UPDATE kimai2_user_preferences SET `name` = 'calendar.initial_view' WHERE `name` = 'calendar_initial_view'");
@@ -112,9 +96,8 @@ final class Version20993112235958 extends AbstractMigration
         $this->addSql("UPDATE kimai2_user_preferences SET `name` = 'timesheet.export_decimal' WHERE `name` = 'export_decimal'");
         $this->addSql("UPDATE kimai2_user_preferences SET `name` = 'theme.update_browser_title' WHERE `name` = 'update_browser_title'");
 
-        $this->addSql('UPDATE kimai2_configuration SET `value` = null WHERE `name` = "defaults.user.theme"');
-        $this->addSql('UPDATE kimai2_user_preferences SET `value` = "fixed" WHERE `name` = "theme.layout"');
-        $this->addSql('UPDATE kimai2_user_preferences SET `value` = null WHERE `name` = "skin"');
-        $this->addSql('UPDATE kimai2_user_preferences SET `value` = "0" WHERE `name` = "theme.collapsed_sidebar"');
+        //$this->addSql('UPDATE kimai2_configuration SET `value` = null WHERE `name` = "defaults.user.theme"');
+        //$this->addSql('UPDATE kimai2_user_preferences SET `value` = "fixed" WHERE `name` = "theme.layout"');
+        //$this->addSql('UPDATE kimai2_user_preferences SET `value` = null WHERE `name` = "skin"');
     }
 }
--- migrations/Version20993112235959.php
+++ migrations/Version20993112235959.php
@@ -16,6 +16,8 @@ use Doctrine\Migrations\AbstractMigration;
 
 /**
  * @version 2.1.0
+ *
+ * FIXME - SEARCH FOR @2.1 TO FIND ALL CODE PIECES THAT CAN BE REMOVED IN COMBINATION WITH THIS MIGRATION!
  */
 final class Version20993112235959 extends AbstractMigration
 {
@@ -26,18 +28,27 @@ final class Version20993112235959 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-//        return;
+        $this->addSql("DELETE FROM kimai2_user_preferences WHERE `name` = 'theme.collapsed_sidebar'");
+        $this->addSql("DELETE FROM kimai2_user_preferences WHERE `name` = 'collapsed_sidebar'");
         $this->addSql("DELETE FROM kimai2_roles_permissions WHERE `permission` LIKE 'comments_create%'");
+        $this->addSql("DELETE FROM kimai2_configuration WHERE `name` = 'timesheet.active_entries.soft_limit'");
+        $this->addSql("DELETE FROM kimai2_user_preferences where `name` = 'reporting.initial_view'");
+        $this->addSql("DELETE FROM kimai2_user_preferences where `name` = 'hours_24'");
+        $this->addSql("DELETE FROM kimai2_configuration WHERE `name` = 'theme.autocomplete_chars'");
+        $this->addSql("DELETE FROM kimai2_configuration WHERE `name` = 'theme.tags_create'");
+        $this->addSql("UPDATE kimai2_configuration SET `value` = 'default' WHERE `name` = 'timesheet.mode' and `value` = 'duration_only'");
+
+        // TODO rename user configuration calendar first day agendaDay = day / agendaMonth = month
 
         $templates = $schema->getTable('kimai2_invoice_templates');
-
-        $templates->getColumn('language')->setNotnull(true);
         $templates->dropColumn('decimal_duration');
+
+        // cannot be moved to the earlier migration, because of the execution order of schema changes and SQL statements
+        $templates->getColumn('language')->setNotnull(true);
     }
 
     public function down(Schema $schema): void
     {
-//        return;
         $templates = $schema->getTable('kimai2_invoice_templates');
 
         $templates->addColumn('decimal_duration', 'boolean', ['notnull' => true, 'default' => false]);
