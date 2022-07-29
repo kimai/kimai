22731e2adfa2603e81c71ad0159f7e967a1eb9fc
Kevin Papst <kpapst@gmx.net>
2022-07-29 15:15:51 +0200

    fix migrations

--- migrations/Version20993112235958.php
+++ migrations/Version20993112235958.php
@@ -61,11 +61,7 @@ final class Version20993112235958 extends AbstractMigration
         $this->addSql("UPDATE kimai2_user_preferences SET `name` = 'calendar_initial_view' WHERE `name` = 'calendar.initial_view'");
         $this->addSql("UPDATE kimai2_user_preferences SET `name` = 'export_decimal' WHERE `name` = 'timesheet.export_decimal'");
         $this->addSql("UPDATE kimai2_user_preferences SET `name` = 'update_browser_title' WHERE `name` = 'theme.update_browser_title'");
-
-        // TODO update SET `value` = '15' kimai2_configuration WHERE `name` = 'timesheet.time_increment' and value = '0'
-        //$this->addSql('UPDATE kimai2_user_preferences SET `value` = "boxed" WHERE `name` = "theme.layout"');
-        //$this->addSql('UPDATE kimai2_user_preferences SET `value` = "default" WHERE `name` = "skin"');
-        //$this->addSql('UPDATE kimai2_configuration SET `value` = "default" WHERE `name` = "defaults.user.theme"');
+        $this->addSql("UPDATE kimai2_configuration SET `value` = '15' WHERE `name` = 'timesheet.time_increment' and `value` = '0'");
     }
 
     public function down(Schema $schema): void
@@ -95,9 +91,5 @@ final class Version20993112235958 extends AbstractMigration
         $this->addSql("UPDATE kimai2_user_preferences SET `name` = 'timesheet.daily_stats' WHERE `name` = 'timesheet_daily_stats'");
         $this->addSql("UPDATE kimai2_user_preferences SET `name` = 'timesheet.export_decimal' WHERE `name` = 'export_decimal'");
         $this->addSql("UPDATE kimai2_user_preferences SET `name` = 'theme.update_browser_title' WHERE `name` = 'update_browser_title'");
-
-        //$this->addSql('UPDATE kimai2_configuration SET `value` = null WHERE `name` = "defaults.user.theme"');
-        //$this->addSql('UPDATE kimai2_user_preferences SET `value` = "fixed" WHERE `name` = "theme.layout"');
-        //$this->addSql('UPDATE kimai2_user_preferences SET `value` = null WHERE `name` = "skin"');
     }
 }
--- migrations/Version20993112235959.php
+++ migrations/Version20993112235959.php
@@ -31,12 +31,17 @@ final class Version20993112235959 extends AbstractMigration
         $this->addSql("DELETE FROM kimai2_user_preferences WHERE `name` = 'theme.collapsed_sidebar'");
         $this->addSql("DELETE FROM kimai2_user_preferences WHERE `name` = 'collapsed_sidebar'");
         $this->addSql("DELETE FROM kimai2_roles_permissions WHERE `permission` LIKE 'comments_create%'");
-        $this->addSql("DELETE FROM kimai2_configuration WHERE `name` = 'timesheet.active_entries.soft_limit'");
+
         $this->addSql("DELETE FROM kimai2_user_preferences where `name` = 'reporting.initial_view'");
         $this->addSql("DELETE FROM kimai2_user_preferences where `name` = 'hours_24'");
+        $this->addSql("UPDATE kimai2_user_preferences SET `value` = 'default' WHERE `name` = 'layout' and `value` = 'fixed'");
+        $this->addSql("UPDATE kimai2_user_preferences SET `value` = 'default' WHERE `name` = 'skin'");
+
+        $this->addSql("DELETE FROM kimai2_configuration WHERE `name` = 'timesheet.active_entries.soft_limit'");
         $this->addSql("DELETE FROM kimai2_configuration WHERE `name` = 'theme.autocomplete_chars'");
         $this->addSql("DELETE FROM kimai2_configuration WHERE `name` = 'theme.tags_create'");
         $this->addSql("UPDATE kimai2_configuration SET `value` = 'default' WHERE `name` = 'timesheet.mode' and `value` = 'duration_only'");
+        $this->addSql("UPDATE kimai2_configuration SET `value` = 'default' WHERE `name` = 'defaults.user.theme'");
 
         // TODO rename user configuration calendar first day agendaDay = day / agendaMonth = month
 
@@ -53,5 +58,11 @@ final class Version20993112235959 extends AbstractMigration
 
         $templates->addColumn('decimal_duration', 'boolean', ['notnull' => true, 'default' => false]);
         $templates->getColumn('language')->setNotnull(false);
+
+        $this->addSql("UPDATE kimai2_user_preferences SET `value` = 'fixed' WHERE `name` = 'layout' and `value` = 'default'");
+
+        // rollback makes it impossible to choose the correct one
+        $this->addSql("DELETE FROM kimai2_user_preferences WHERE `name` = 'skin'");
+        $this->addSql("DELETE FROM kimai2_configuration WHERE `name` = 'defaults.user.theme'");
     }
 }
