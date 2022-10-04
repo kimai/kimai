2081f3c5cdba1f00c39f60a97e8946fd8eb48193
Kevin Papst <kpapst@gmx.net>
2022-10-04 23:41:46 +0200

    rebuild after update

--- assets/js/plugins/KimaiAjaxModalForm.js
+++ assets/js/plugins/KimaiAjaxModalForm.js
@@ -125,7 +125,7 @@ export default class KimaiAjaxModalForm extends KimaiReducedClickHandler {
         if (node.tagName !== undefined && node.tagName === 'SCRIPT') {
             const script  = document.createElement('script');
             script.text = node.innerHTML;
-            node.parentNode.replaceChild(script, node );
+            node.parentNode.replaceChild(script, node);
         } else {
             for (const child of node.childNodes) {
                 this._makeScriptExecutable(child);
