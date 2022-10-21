24a31a44bbc12737fb3e82b6be13484b2c48b8e2
Kevin Papst <kpapst@gmx.net>
2022-10-21 11:19:28 +0200

    fix bool user preference was required

--- src/Form/Type/UserPreferenceType.php
+++ src/Form/Type/UserPreferenceType.php
@@ -46,7 +46,7 @@ class UserPreferenceType extends AbstractType
                     unset($options['required']);
                 }
 
-                if (CheckboxType::class === $preference->getType()) {
+                if (in_array($preference->getType(), [CheckboxType::class, YesNoType::class])) {
                     $required = false;
                 }
 
