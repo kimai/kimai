cee4061110ad42a247cd9753dea23c7cb15a5a0a
Kevin Papst <kpapst@gmx.net>
2022-10-24 17:53:24 +0200

    code style

--- assets/js/plugins/KimaiHotkeys.js
+++ assets/js/plugins/KimaiHotkeys.js
@@ -1,23 +1,32 @@
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 import KimaiPlugin from "../KimaiPlugin";
 
 export default class KimaiHotkeys extends KimaiPlugin {
-    getId() {
+
+    getId()
+    {
         return 'hotkeys';
     }
 
-    init() {
+    init()
+    {
         // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
         // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key
 
-        const selector = `[data-hotkey="ctrl+Enter"]`
-        const matches = ev => ev.ctrlKey && ev.key === 'Enter'
+        const selector = '[data-hotkey="ctrl+Enter"]';
 
         window.addEventListener('keyup', (ev) => {
-            if (matches(ev)) {
-                const elements = [...document.querySelectorAll(selector)].filter(element => isVisible(element))
+            if (ev.ctrlKey && ev.key === 'Enter') {
+                const elements = [...document.querySelectorAll(selector)].filter(element => this.isVisible(element));
 
                 if (elements.length > 1) {
-                    console.warn(`KimaiHotkeys: More than one visible element matches ${selector}. No action triggered.`)
+                    console.warn('KimaiHotkeys: More than one visible element matches ${selector}. No action triggered.');
                 }
 
                 if (elements.length === 1) {
@@ -29,13 +38,14 @@ export default class KimaiHotkeys extends KimaiPlugin {
             }
         })
     }
-}
 
-// adopted from Bootstrap 5.1.1, MIT
-function isVisible (element) {
-    if (!element || element.getClientRects().length === 0) {
-        return false;
+    // adopted from Bootstrap 5.1.1, MIT
+    isVisible (element)
+    {
+        if (!element || element.getClientRects().length === 0) {
+            return false;
+        }
+
+        return getComputedStyle(element).getPropertyValue('visibility') === 'visible';
     }
-
-    return getComputedStyle(element).getPropertyValue('visibility') === 'visible';
 }
