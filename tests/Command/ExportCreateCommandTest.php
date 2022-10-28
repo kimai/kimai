897345588f11d3bb6b132ccdddaabfb6ac43c873
Kevin Papst <kpapst@gmx.net>
2022-10-29 00:13:02 +0200

    added tests

--- tests/Command/ExportCreateCommandTest.php
+++ tests/Command/ExportCreateCommandTest.php
@@ -222,7 +222,7 @@ class ExportCreateCommandTest extends KernelTestCase
 
         $data = $this->prepareFixtures($start);
 
-        $commandTester = $this->createExport(['--template' => 'csv', '--project' => [$data[1]->getId()], '--start' => $start->format('Y-m-d'), '--end' => $end->format('Y-m-d')]);
+        $commandTester = $this->createExport(['--template' => 'csv', '--project' => [$data[1][0]->getId()], '--start' => $start->format('Y-m-d'), '--end' => $end->format('Y-m-d')]);
 
         $output = $commandTester->getDisplay();
         $this->assertStringContainsString('Saved export to: ', $output);
