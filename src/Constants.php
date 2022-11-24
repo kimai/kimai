072b64989f697de04104d706b085f99b3aab0320
Kevin Papst <kpapst@gmx.net>
2022-11-24 01:07:42 +0100

    patch release

--- src/Constants.php
+++ src/Constants.php
@@ -17,11 +17,11 @@ class Constants
     /**
      * The current release version
      */
-    public const VERSION = '1.30.0';
+    public const VERSION = '1.29.1';
     /**
      * The current release: major * 10000 + minor * 100 + patch
      */
-    public const VERSION_ID = 13000;
+    public const VERSION_ID = 12901;
     /**
      * The current release status, either "stable" or "dev"
      */
