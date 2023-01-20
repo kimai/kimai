db4179df1952f7677399f611a14a2ae4681ccca7
Kevin Papst <kevinpapst@users.noreply.github.com>
2023-01-20 12:55:50 +0100

    release 1.30.5 (#3783)

--- src/Constants.php
+++ src/Constants.php
@@ -17,11 +17,11 @@ class Constants
     /**
      * The current release version
      */
-    public const VERSION = '1.30.4';
+    public const VERSION = '1.30.5';
     /**
      * The current release: major * 10000 + minor * 100 + patch
      */
-    public const VERSION_ID = 13004;
+    public const VERSION_ID = 13005;
     /**
      * The current release status, either "stable" or "dev"
      */
