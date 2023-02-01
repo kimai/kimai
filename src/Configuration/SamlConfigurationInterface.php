d898040b716c849aa40c48468b6ef5dc00d14301
Kevin Papst <kpapst@gmx.net>
2023-02-01 17:02:36 +0100

    phpdoc

--- src/Configuration/SamlConfigurationInterface.php
+++ src/Configuration/SamlConfigurationInterface.php
@@ -9,12 +9,32 @@
 
 namespace App\Configuration;
 
+/**
+ * @CloudRequired
+ */
 interface SamlConfigurationInterface
 {
+    /**
+     * Whether SAML login is activated.
+     *
+     * @return bool
+     */
     public function isActivated(): bool;
 
+    /**
+     * Returns the title  that is exclusively used in the frontend.
+     * Currently, used to display the button in the login screen.
+     *
+     * @return string
+     */
     public function getTitle(): string;
 
+    /**
+     * Returns the provider name that is exclusively used in the frontend.
+     * Currently, used to display an icon in the login screen.
+     *
+     * @return string
+     */
     public function getProvider(): string;
 
     public function getAttributeMapping(): array;
