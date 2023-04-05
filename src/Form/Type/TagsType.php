d9fc279ffd8512195e49ed3554633270a0267788
Kevin Papst <kpapst@gmx.net>
2023-04-05 18:10:23 +0200

    cache tag amount oer request

--- src/Form/Type/TagsType.php
+++ src/Form/Type/TagsType.php
@@ -16,6 +16,8 @@ use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 final class TagsType extends AbstractType
 {
+    private static ?int $count = null;
+
     public function __construct(
         private AuthorizationCheckerInterface $auth,
         private TagRepository $repository
@@ -31,7 +33,11 @@ final class TagsType extends AbstractType
 
     public function getParent(): string
     {
-        if ($this->repository->count([]) > TagRepository::MAX_AMOUNT_SELECT) {
+        if (self::$count === null) {
+            self::$count = $this->repository->count([]);
+        }
+
+        if (self::$count > TagRepository::MAX_AMOUNT_SELECT) {
             return TagsInputType::class;
         }
 
