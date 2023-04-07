9e33ab007ce624ce84660c2630fd86bda65ac95a
Kevin Papst <kpapst@gmx.net>
2023-04-07 13:15:46 +0200

    Release-2.0.15

--- src/Constants.php
+++ src/Constants.php
@@ -17,11 +17,11 @@ class Constants
     /**
      * The current release version
      */
-    public const VERSION = '2.0.14';
+    public const VERSION = '2.0.15';
     /**
      * The current release: major * 10000 + minor * 100 + patch
      */
-    public const VERSION_ID = 20014;
+    public const VERSION_ID = 20015;
     /**
      * The software name
      */
