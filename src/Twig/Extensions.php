d30c51f3784280c68aaa6e1c6bcf14f0289a9ba9
Kevin Papst <kpapst@gmx.net>
2023-04-09 17:50:08 +0200

    support action for working time months

--- src/Twig/Extensions.php
+++ src/Twig/Extensions.php
@@ -19,6 +19,8 @@ use Twig\TwigTest;
 
 final class Extensions extends AbstractExtension
 {
+    public const REPORT_DATE = 'Y-m-d';
+
     /**
      * {@inheritdoc}
      */
@@ -58,7 +60,7 @@ final class Extensions extends AbstractExtension
 
     public function formatReportDate(\DateTimeInterface $dateTime): string
     {
-        return $dateTime->format('Y-m-d');
+        return $dateTime->format(self::REPORT_DATE);
     }
 
     public function getIsoDayByName(string $weekDay): int
