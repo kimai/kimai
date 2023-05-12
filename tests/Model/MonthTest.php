808ee30a6cceca1ee43e3798938df359696bc13c
Kevin Papst <kpapst@gmx.net>
2023-05-12 23:17:13 +0200

    added tests

---
+++ tests/Model/MonthTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Model;
+
+use App\Model\Month;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \App\Model\Month
+ */
+class MonthTest extends TestCase
+{
+    public function testDefaults(): void
+    {
+        $months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        foreach($months as $key => $days) {
+            $index = ++$key;
+            $monthKey = ($index < 10) ? '0' . $index : $index;
+            $date = new \DateTimeImmutable(sprintf('2020-%s-25 13:00:00', $monthKey));
+            $month = new Month($date);
+            self::assertEquals(sprintf('2020-%s-25', $monthKey), $month->getMonth()->format('Y-m-d'));
+            self::assertCount($days, $month->getDays());
+        }
+    }
+}
