2ac050ae82c0190e66cf7f54f78278008a9c121a
Kevin Papst <kpapst@gmx.net>
2023-07-09 15:04:28 +0200

    added first day of work support

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1174,6 +1174,25 @@ class User implements UserInterface, EquatableInterface, ThemeUserInterface, Pas
         return (int) $this->getPreferenceValue(UserPreference::WORK_HOURS_SUNDAY, 0);
     }
 
+    public function getWorkStartingDay(): ?\DateTimeInterface
+    {
+        $date = $this->getPreferenceValue(UserPreference::WORK_STARTING_DAY);
+        if ($date === null) {
+            return null;
+        }
+
+        try {
+            return \DateTimeImmutable::createFromFormat('Y-m-d h:i:s', $date . ' 00:00:00', new \DateTimeZone($this->getTimezone()));
+        } catch (Exception $e) {
+            return null;
+        }
+    }
+
+    public function setWorkStartingDay(?\DateTimeInterface $date): void
+    {
+        $this->setPreferenceValue(UserPreference::WORK_STARTING_DAY, $date?->format('Y-m-d'));
+    }
+
     public function getPublicHolidayGroup(): null|string
     {
         $group = $this->getPreferenceValue(UserPreference::PUBLIC_HOLIDAY_GROUP);
