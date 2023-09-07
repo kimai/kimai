75825f925abd2a23368970b4be2b20ebe5e7262d
Kevin Papst <kpapst@gmx.net>
2023-09-07 23:54:46 +0200

    bump version

--- src/Constants.php
+++ src/Constants.php
@@ -17,11 +17,11 @@ class Constants
     /**
      * The current release version
      */
-    public const VERSION = '2.0.33';
+    public const VERSION = '2.0.34';
     /**
      * The current release: major * 10000 + minor * 100 + patch
      */
-    public const VERSION_ID = 20033;
+    public const VERSION_ID = 20034;
     /**
      * The software name
      */
