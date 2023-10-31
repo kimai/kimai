b331715953580a178a95dc80f79a66c38ec52ede
Kevin Papst <kpapst@gmx.net>
2023-10-31 16:25:54 +0100

    todo

--- src/Entity/Timesheet.php
+++ src/Entity/Timesheet.php
@@ -189,7 +189,7 @@ class Timesheet implements EntityWithMetaFields, ExportableItem
     #[Assert\NotNull]
     private ?string $category = self::WORK;
     #[ORM\Column(name: 'modified_at', type: 'datetime_immutable', nullable: true)]
-    private \DateTimeImmutable $modifiedAt; // @phpstan-ignore-line
+    private \DateTimeImmutable $modifiedAt; // @phpstan-ignore-line - create migration and update all null values and then make it not null
     /**
      * Tags
      *
