4ba96dd3f635e49faf1aae6ad8a00a3e472a542b
Kevin Papst <kpapst@gmx.net>
2023-12-26 14:51:46 +0100

    prepare next release branch

--- src/Constants.php
+++ src/Constants.php
@@ -17,11 +17,11 @@ class Constants
     /**
      * The current release version
      */
-    public const VERSION = '2.7.0';
+    public const VERSION = '2.8.0';
     /**
      * The current release: major * 10000 + minor * 100 + patch
      */
-    public const VERSION_ID = 20700;
+    public const VERSION_ID = 20800;
     /**
      * The software name
      */
