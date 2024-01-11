9b2c7f13f3bbd8c271619ec93337a54b161ce691
Kevin Papst <kpapst@gmx.net>
2024-01-11 18:48:24 +0100

    remove unused methods

--- src/Repository/Query/UserFormTypeQuery.php
+++ src/Repository/Query/UserFormTypeQuery.php
@@ -9,64 +9,10 @@
 
 namespace App\Repository\Query;
 
-use App\Entity\User;
-
 /**
  * Can be used to pre-fill form types with: UserRepository::getQueryBuilderForFormType()
  */
 final class UserFormTypeQuery extends BaseFormTypeQuery
 {
     use VisibilityTrait;
-
-    /**
-     * @var User[]
-     */
-    private array $includeUsers = [];
-    /**
-     * @var User[]
-     */
-    private array $ignoredUsers = [];
-
-    /**
-     * Sets a list of users which must be included in the result always.
-     *
-     * @param array<User> $users
-     */
-    public function setUsersAlwaysIncluded(array $users): void
-    {
-        $this->includeUsers = $users;
-    }
-
-    /**
-     * Get the list of users which should always be included in the result.
-     *
-     * @return User[]
-     */
-    public function getUsersAlwaysIncluded(): array
-    {
-        return $this->includeUsers;
-    }
-
-    /**
-     * Given user will be excluded from the result set.
-     *
-     * @param User $user
-     * @return $this
-     */
-    public function addUserToIgnore(User $user): UserFormTypeQuery
-    {
-        $this->ignoredUsers[] = $user;
-
-        return $this;
-    }
-
-    /**
-     * Returns the list of users that should not be loaded.
-     *
-     * @return User[]
-     */
-    public function getUsersToIgnore(): array
-    {
-        return $this->ignoredUsers;
-    }
 }
