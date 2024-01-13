22b684dc1927092735d3f8499940be09a5b972e8
Ronald Derksen <ronald@derksen-it.nl>
2024-01-13 13:24:58 +0100

    Make 'pagebreak' optional in pdf layout exporter

---
+++ cp.sh
@@ -0,0 +1,4 @@
+cp templates/export/renderer/derksen-it-timesheet.pdf.twig \
+   templates/invoice/renderer/derksen-it.pdf.twig \
+   templates/export/pdf-layout.html.twig \
+   /net/192.168.12.120/volume1/docker/kimai/
