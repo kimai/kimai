e36667f198c15609be308ad99e9477c92843bdfe
Kevin Papst <kpapst@gmx.net>
2024-01-17 22:36:38 +0100

    code style

--- src/EventSubscriber/EmailSubscriber.php
+++ src/EventSubscriber/EmailSubscriber.php
@@ -18,7 +18,7 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
  */
 final class EmailSubscriber implements EventSubscriberInterface
 {
-    public function __construct(private KimaiMailer $mailer)
+    public function __construct(private readonly KimaiMailer $mailer)
     {
     }
 
