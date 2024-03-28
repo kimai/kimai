c1893a2c5147acc94704ca11af9f8a57f4c21f9d
Kevin Papst <kpapst@gmx.net>
2024-03-28 18:43:24 +0100

    better configuration of dropdown pattern for customer, project and activity

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -11,6 +11,9 @@ namespace App\DependencyInjection;
 
 use App\Entity\Customer;
 use App\Entity\User;
+use App\Form\Helper\ActivityHelper;
+use App\Form\Helper\CustomerHelper;
+use App\Form\Helper\ProjectHelper;
 use App\Repository\InvoiceDocumentRepository;
 use App\Timesheet\Rounding\RoundingInterface;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
@@ -114,6 +117,9 @@ final class Configuration implements ConfigurationInterface
         $node
             ->addDefaultsIfNotSet()
             ->children()
+                ->scalarNode('choice_pattern')
+                    ->defaultValue(ProjectHelper::PATTERN_NAME)
+                ->end()
                 ->booleanNode('copy_teams_on_create')
                     ->defaultValue(false)
                 ->end()
@@ -138,6 +144,9 @@ final class Configuration implements ConfigurationInterface
         $node
             ->addDefaultsIfNotSet()
             ->children()
+                ->scalarNode('choice_pattern')
+                    ->defaultValue(ActivityHelper::PATTERN_NAME)
+                ->end()
                 ->booleanNode('allow_inline_create')
                     ->defaultValue(false)
                 ->end()
@@ -563,6 +572,9 @@ final class Configuration implements ConfigurationInterface
         $node
             ->addDefaultsIfNotSet()
             ->children()
+                ->scalarNode('choice_pattern')
+                    ->defaultValue(CustomerHelper::PATTERN_NAME)
+                ->end()
                 ->scalarNode('number_format')
                     ->defaultValue('{cc,4}')
                 ->end()
