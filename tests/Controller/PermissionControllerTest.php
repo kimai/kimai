ffe9f3f9d6eebe298b29e15453a7caa3d1fea099
Kevin Papst <kpapst@gmx.net>
2024-04-13 16:56:07 +0200

    fix test

--- tests/Controller/PermissionControllerTest.php
+++ tests/Controller/PermissionControllerTest.php
@@ -34,7 +34,7 @@ class PermissionControllerTest extends ControllerBaseTest
         $client = $this->getClientForAuthenticatedUser(User::ROLE_SUPER_ADMIN);
         $this->assertAccessIsGranted($client, '/admin/permissions');
         $this->assertHasDataTable($client);
-        $this->assertDataTableRowCount($client, 'datatable_user_admin_permissions', 132);
+        $this->assertDataTableRowCount($client, 'datatable_user_admin_permissions', 133);
         $this->assertPageActions($client, [
             'create modal-ajax-form' => $this->createUrl('/admin/permissions/roles/create'),
         ]);
