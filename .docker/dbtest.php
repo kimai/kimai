665c26f730ee20e0b0eea0a566c681242a21a881
Kevin Papst <kevinpapst@users.noreply.github.com>
2024-08-04 15:59:45 +0200

    Update .docker/dbtest.php

--- .docker/dbtest.php
+++ .docker/dbtest.php
@@ -2,8 +2,8 @@
 $DB_HOST = urldecode($argv[1]);
 $DB_BASE = urldecode($argv[2]);
 $DB_PORT = $argv[3];
-$DB_USER = urldecode(str_replace('+', '%2B', $argv[4]));
-$DB_PASS = urldecode(str_replace('+', '%2B', $argv[5]));
+$DB_USER = urldecode($argv[4]);
+$DB_PASS = urldecode($argv[5]);
 
 echo "Testing DB:";
 echo "*";
