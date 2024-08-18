1df48ca5ad207276647993349a9dd8d28534b8bd
Kevin Papst <kpapst@gmx.net>
2024-08-18 19:15:55 +0200

    phpdoc

--- src/Repository/CustomerRepository.php
+++ src/Repository/CustomerRepository.php
@@ -120,6 +120,8 @@ class CustomerRepository extends EntityRepository
 
     /**
      * Returns a query builder that is used for CustomerType and your own 'query_builder' option.
+     *
+     * @internal
      */
     public function getQueryBuilderForFormType(CustomerFormTypeQuery $query): QueryBuilder
     {
