b8c85661aaac41ab9f1a48e04abd7709370814f8
Kevin Papst <kpapst@gmx.net>
2024-09-22 21:08:03 +0200

    cleanup environment variables, remove deprecated single parameter

--- .docker/service.sh
+++ .docker/service.sh
@@ -2,30 +2,19 @@
 
 function waitForDB() {
   # Parse sql connection data
-  if [ ! -z "$DATABASE_URL" ]; then
-    DB_TYPE=$(awk -F '[/:@]' '{print $1}' <<< "$DATABASE_URL")
-    DB_USER=$(awk -F '[/:@]' '{print $4}' <<< "$DATABASE_URL")
-    DB_PASS=$(awk -F '[/:@]' '{print $5}' <<< "$DATABASE_URL")
-    DB_HOST=$(awk -F '[/:@]' '{print $6}' <<< "$DATABASE_URL")
-    DB_PORT=$(awk -F '[/:@]' '{print $7}' <<< "$DATABASE_URL")
-    DB_BASE=$(awk -F '[/?]' '{print $4}' <<< "$DATABASE_URL")
-  else
-    DB_TYPE=${DB_TYPE:mysql}
-    if [ "$DB_TYPE" == "mysql" ]; then
-      export DATABASE_URL="${DB_TYPE}://${DB_USER:=kimai}:${DB_PASS:=kimai}@${DB_HOST:=sqldb}:${DB_PORT:=3306}/${DB_BASE:=kimai}"
-    else
-      echo "Unknown database type, cannot proceed. Only 'mysql' is supported, received: [$DB_TYPE]"
-      exit 1
-    fi
-  fi
+  DATABASE_USER=$(awk -F '[/:@]' '{print $4}' <<< "$DATABASE_URL")
+  DATABASE_PASS=$(awk -F '[/:@]' '{print $5}' <<< "$DATABASE_URL")
+  DATABASE_HOST=$(awk -F '[/:@]' '{print $6}' <<< "$DATABASE_URL")
+  DATABASE_PORT=$(awk -F '[/:@]' '{print $7}' <<< "$DATABASE_URL")
+  DATABASE_BASE=$(awk -F '[/?]' '{print $4}' <<< "$DATABASE_URL")
 
   re='^[0-9]+$'
-  if ! [[ $DB_PORT =~ $re ]] ; then
-     DB_PORT=3306
+  if ! [[ $DATABASE_PORT =~ $re ]] ; then
+     DATABASE_PORT=3306
   fi
 
-  echo "Wait for MySQL DB connection ..."
-  until php /dbtest.php $DB_HOST $DB_BASE $DB_PORT $DB_USER $DB_PASS; do
+  echo "Wait for database connection ..."
+  until php /dbtest.php "$DATABASE_HOST" "$DATABASE_BASE" "$DATABASE_PORT" "$DATABASE_USER" "$DATABASE_PASS"; do
     echo Checking DB: $?
     sleep 3
   done
@@ -33,14 +22,14 @@ function waitForDB() {
 }
 
 function handleStartup() {
-  # These are idempotent, run them anyway
+  # These are idempotent, so we can run them on every start-up
   /opt/kimai/bin/console -n kimai:install
   /opt/kimai/bin/console -n kimai:update
   if [ ! -z "$ADMINPASS" ] && [ ! -a "$ADMINMAIL" ]; then
-    /opt/kimai/bin/console kimai:user:create superadmin "$ADMINMAIL" ROLE_SUPER_ADMIN "$ADMINPASS"
+    /opt/kimai/bin/console kimai:user:create admin "$ADMINMAIL" ROLE_SUPER_ADMIN "$ADMINPASS"
   fi
   echo "$KIMAI" > /opt/kimai/var/installed
-  echo "Kimai2 ready"
+  echo "Kimai is ready"
 }
 
 function runServer() {
