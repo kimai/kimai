8d2a858b85fdffe387b6fe6e273cfc9c3ecf9017
Kevin Papst <kevin@kevinpapst.de>
2025-05-06 20:48:30 +0200

    version

--- src/Constants.php
+++ src/Constants.php
@@ -21,7 +21,7 @@ final class Constants
     /**
      * The current release: major * 10000 + minor * 100 + patch
      */
-    public const VERSION_ID = 23300;
+    public const VERSION_ID = 23400;
     /**
      * The software name
      */
