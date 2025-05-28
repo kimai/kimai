3f550698fdf7cd7cc28f76ad0b1ceabdb5bded75
Kevin Papst <kevin@kevinpapst.de>
2025-05-28 11:24:44 +0200

    use PhpSubprocess to execute migrations

--- src/Command/ResetDevelopmentCommand.php
+++ src/Command/ResetDevelopmentCommand.php
@@ -10,9 +10,10 @@
 namespace App\Command;
 
 use Symfony\Component\Console\Attribute\AsCommand;
-use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Process\PhpSubprocess;
 
 /**
  * Command used to execute all the basic application bootstrapping AFTER "composer install" was executed.
@@ -24,16 +25,36 @@ use Symfony\Component\Console\Output\OutputInterface;
 #[AsCommand(name: 'kimai:reset:dev', description: 'Resets the "development" environment')]
 final class ResetDevelopmentCommand extends AbstractResetCommand
 {
-    public function __construct(string $kernelEnvironment)
+    public function __construct(string $kernelEnvironment, private readonly string $projectDirectory)
     {
         parent::__construct($kernelEnvironment);
     }
 
     protected function loadData(InputInterface $input, OutputInterface $output): void
     {
-        $command = $this->getApplication()->find('doctrine:fixtures:load');
-        $cmdInput = new ArrayInput([]);
-        $cmdInput->setInteractive(false);
-        $command->run($cmdInput, $output);
+        $io = new SymfonyStyle($input, $output);
+
+        $io->writeln('Importing fixtures, this will take a while ... please be patient ...');
+
+        $process = new PhpSubprocess(
+            [
+                'bin/console',
+                'doctrine:fixtures:load',
+                '--no-interaction',
+            ],
+            $this->projectDirectory
+        );
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            $io->error('Failed to load fixtures');
+            $io->error($process->getErrorOutput());
+        } else {
+            if ($io->isVerbose()) {
+                $io->write($process->getOutput());
+            } else {
+                $io->success('Fixtures loaded');
+            }
+        }
     }
 }
