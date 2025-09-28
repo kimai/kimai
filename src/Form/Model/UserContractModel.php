46b8f6eebc7bea29e548c45a5c3e69df0fec3963
Kevin Papst <kevin@kevinpapst.de>
2025-09-28 22:51:05 +0200

    phpstan with 8.4

--- src/Form/Model/UserContractModel.php
+++ src/Form/Model/UserContractModel.php
@@ -17,7 +17,7 @@ final class UserContractModel
     {
     }
 
-    public function __isset(string $name): true
+    public function __isset(string $name): true // @phpstan-ignore return.tooWideBool
     {
         return true;
     }
