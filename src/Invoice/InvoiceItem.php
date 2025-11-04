33bd5d3a26ad65251c7816a8fcb3806ca62c817f
Kevin Papst <kevin@kevinpapst.de>
2025-11-04 20:28:22 +0100

    deprecate internal rate

--- src/Invoice/InvoiceItem.php
+++ src/Invoice/InvoiceItem.php
@@ -19,6 +19,9 @@ final class InvoiceItem
     private ?float $fixedRate = null;
     private ?float $hourlyRate = null;
     private float $rate = 0.00;
+    /**
+     * @deprecated since 2.41 - internal rate is not needed in invoices
+     */
     private float $rateInternal = 0.00;
     private float $amount = 0.00;
     private ?string $description = null;
@@ -126,11 +129,17 @@ final class InvoiceItem
         return $this;
     }
 
+    /**
+     * @deprecated since 2.41 - internal rate is not needed in invoices
+     */
     public function getInternalRate(): float
     {
         return $this->rateInternal;
     }
 
+    /**
+     * @deprecated since 2.41 - internal rate is not needed in invoices
+     */
     public function setInternalRate(float $rateInternal): InvoiceItem
     {
         $this->rateInternal = $rateInternal;
