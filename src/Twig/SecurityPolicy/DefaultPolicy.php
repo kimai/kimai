686473c416971e3cd3295b534645e1e5fd109ef8
Kevin Papst <kevin@kevinpapst.de>
2026-01-06 00:18:29 +0100

    add security checks to prevent access to sensitive data in export and invoice environment

--- src/Twig/SecurityPolicy/DefaultPolicy.php
+++ src/Twig/SecurityPolicy/DefaultPolicy.php
@@ -9,6 +9,13 @@
 
 namespace App\Twig\SecurityPolicy;
 
+use App\Entity\User;
+use App\Pdf\PdfContext;
+use Symfony\Bridge\Twig\AppVariable;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ServerBag;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Twig\Sandbox\SecurityNotAllowedPropertyError;
 use Twig\Sandbox\SecurityPolicyInterface;
 
 /**
@@ -22,6 +29,49 @@ final class DefaultPolicy implements SecurityPolicyInterface
 
     public function checkMethodAllowed($obj, $method): void
     {
+        if ($obj instanceof ServerBag) {
+            throw new SecurityNotAllowedPropertyError('Tried to access server environment', ServerBag::class, $method);
+        }
+
+        if ($obj instanceof SessionInterface) {
+            throw new SecurityNotAllowedPropertyError('Tried to access session', SessionInterface::class, $method);
+        }
+
+        $lcm = strtolower($method);
+
+        if ($obj instanceof PdfContext) {
+            if ($lcm !== 'setoption') {
+                throw new SecurityNotAllowedPropertyError('Tried to access forbidden method on PdfContext', PdfContext::class, $method);
+            }
+
+            return;
+        }
+
+        if (!str_starts_with($lcm, 'has') && !str_starts_with($lcm, 'is') && !str_starts_with($lcm, 'get')) {
+            throw new SecurityNotAllowedPropertyError('Tried to access non-read method', $obj::class, $method);
+        }
+
+        if ($obj instanceof Request) {
+            if (!str_starts_with($lcm, 'get')) {
+                throw new SecurityNotAllowedPropertyError('Tried to call setter() of app variable', AppVariable::class, $method);
+            }
+
+            return;
+        }
+
+        if ($obj instanceof AppVariable) {
+            if (!\in_array($lcm, ['getrequest', 'getuser', 'getlocale'], true)) {
+                throw new SecurityNotAllowedPropertyError('Tried to access forbidden app variable method', User::class, $method);
+            }
+
+            return;
+        }
+
+        if ($obj instanceof User) {
+            if (\in_array($lcm, ['getpassword', 'gettotpsecret', 'getplainpassword', 'getconfirmationtoken', 'gettotpauthenticationconfiguration'], true)) {
+                throw new SecurityNotAllowedPropertyError('Tried to access user secrets', User::class, $method);
+            }
+        }
     }
 
     public function checkPropertyAllowed($obj, $property): void
---
+++ tests/Twig/SecurityPolicy/DefaultPolicyTest.php
@@ -0,0 +1,89 @@
+<?php
+
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Twig\SecurityPolicy;
+
+use App\Entity\User;
+use App\Pdf\PdfContext;
+use App\Twig\SecurityPolicy\DefaultPolicy;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use Symfony\Bridge\Twig\AppVariable;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ServerBag;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Twig\Sandbox\SecurityNotAllowedPropertyError;
+
+#[CoversClass(DefaultPolicy::class)]
+class DefaultPolicyTest extends TestCase
+{
+    public function testCheckSecurity(): void
+    {
+        $sut = new DefaultPolicy();
+        $sut->checkSecurity([], [], []);
+        self::assertTrue(true);
+    }
+
+    public function testCheckPropertyAllowed(): void
+    {
+        $sut = new DefaultPolicy();
+        $sut->checkPropertyAllowed(new \stdClass(), 'foo');
+        self::assertTrue(true);
+    }
+
+    #[DataProvider('getCheckMethodAllowedData')]
+    public function testCheckMethodAllowed($obj, string $method, ?string $expectedExceptionMessage = null, ?string $expectedClass = null): void
+    {
+        $sut = new DefaultPolicy();
+
+        if ($expectedExceptionMessage !== null) {
+            $this->expectException(SecurityNotAllowedPropertyError::class);
+            $this->expectExceptionMessage($expectedExceptionMessage);
+        }
+
+        $sut->checkMethodAllowed($obj, $method);
+
+        if ($expectedExceptionMessage === null) {
+            self::assertTrue(true);
+        }
+    }
+
+    public function getCheckMethodAllowedData(): array
+    {
+        return [
+            [new ServerBag(), 'get', 'Tried to access server environment'],
+            [$this->createMock(SessionInterface::class), 'getId', 'Tried to access session'],
+            [new \stdClass(), 'foo', 'Tried to access non-read method'],
+            [new \stdClass(), 'setFoo', 'Tried to access non-read method'],
+            [new \stdClass(), 'getFoo'],
+            [new \stdClass(), 'hasFoo'],
+            [new \stdClass(), 'isFoo'],
+            // Request
+            [new Request(), 'get', null],
+            [new Request(), 'isXmlHttpRequest', 'Tried to call setter() of app variable'],
+            [new Request(), 'hasSession', 'Tried to call setter() of app variable'],
+            // PdfContext
+            [new PdfContext(), 'setOption'],
+            [new PdfContext(), 'getOption', 'Tried to access forbidden method on PdfContext'],
+            // AppVariable
+            [new AppVariable(), 'getRequest'],
+            [new AppVariable(), 'getUser'],
+            [new AppVariable(), 'getLocale'],
+            [new AppVariable(), 'getCharset', 'Tried to access forbidden app variable method'],
+            // User
+            [new User(), 'getUsername'],
+            [new User(), 'getPassword', 'Tried to access user secrets'],
+            [new User(), 'getTotpSecret', 'Tried to access user secrets'],
+            [new User(), 'getPlainPassword', 'Tried to access user secrets'],
+            [new User(), 'getConfirmationToken', 'Tried to access user secrets'],
+            [new User(), 'getTotpAuthenticationConfiguration', 'Tried to access user secrets'],
+        ];
+    }
+}
