162acf37a81c821c57b91ceacfe9847215262636
Kevin Papst <kevin@kevinpapst.de>
2026-01-07 15:29:16 +0100

    phpstan

--- src/Audit/Loggable.php
+++ src/Audit/Loggable.php
@@ -14,7 +14,6 @@ final class Loggable
 {
     /**
      * @param class-string|null $customFieldClass
-     * @param string[] $ignoredProperties
      */
     public function __construct(public ?string $customFieldClass = null)
     {
