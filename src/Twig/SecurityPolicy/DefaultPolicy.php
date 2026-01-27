7b4d1658a0e01155c01a6bd63b9efe98036e6e78
Kevin Papst <kevin@kevinpapst.de>
2026-01-27 15:57:26 +0100

    re-add missing classes

---
+++ src/Twig/SecurityPolicy/DefaultPolicy.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Twig\SecurityPolicy;
+
+use App\Entity\MetaTableTypeInterface;
+use App\Entity\User;
+use App\Pdf\PdfContext;
+use Symfony\Bridge\Twig\AppVariable;
+use Symfony\Component\DependencyInjection\Attribute\Exclude;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ServerBag;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\String\UnicodeString;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Sandbox\SecurityNotAllowedMethodError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityPolicyInterface;
+
+/**
+ * @deprecated since 2.47.0 - use StrictPolicy instead
+ */
+#[Exclude]
+final class DefaultPolicy implements SecurityPolicyInterface
+{
+    private SecurityPolicyInterface $securityPolicy;
+
+    public function __construct()
+    {
+        $this->securityPolicy = new StrictPolicy();
+    }
+
+    public function checkSecurity($tags, $filters, $functions): void
+    {
+        $this->securityPolicy->checkSecurity($tags, $filters, $functions);
+    }
+
+    public function checkMethodAllowed($obj, $method): void
+    {
+        $this->securityPolicy->checkMethodAllowed($obj, $method);
+    }
+
+    public function checkPropertyAllowed($obj, $property): void
+    {
+        $this->securityPolicy->checkPropertyAllowed($obj, $property);
+    }
+}
