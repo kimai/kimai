a1347c7f1614c4014e7d23e6d01500852f550888
Kevin Papst <kevin@kevinpapst.de>
2026-05-01 11:02:19 +0200

    fix deprecation

--- src/Voter/InvoiceVoter.php
+++ src/Voter/InvoiceVoter.php
@@ -13,6 +13,7 @@ use App\Entity\Invoice;
 use App\Entity\User;
 use App\Security\RolePermissionManager;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Vote;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
 /**
@@ -50,7 +51,7 @@ final class InvoiceVoter extends Voter
         return $subject instanceof Invoice && $this->supportsAttribute($attribute);
     }
 
-    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token, ?Vote $vote = null): bool
     {
         $user = $token->getUser();
 
