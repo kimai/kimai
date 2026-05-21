4a76e4d71b3788b553329b59c2edf333aa7dbbd3
Kevin Papst <kevin@kevinpapst.de>
2026-05-21 16:53:22 +0200

    prepare comment post API

---
+++ src/Form/API/CommentApiForm.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Form\API;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class CommentApiForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('pinned', CheckboxType::class, [
+            'required' => false,
+            'documentation' => [
+                'default' => false,
+                'description' => 'Pinned comments always appear first'
+            ],
+        ]);
+
+        $builder->add('message', TextareaType::class, [
+            'label' => false,
+            'documentation' => [
+                'default' => false,
+                'description' => 'The actual comment (markdown supported)'
+            ],
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'csrf_protection' => false,
+        ]);
+    }
+}
