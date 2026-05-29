85b244b321701fc887b70a983c93c0fba53988ab
Ruben Barkow-Kuder <github@r.z11.de>
2026-05-29 13:03:31 +0200

    Remove obsolete activity.allow_inline_create configuration option from SystemConfigurationController
    - Remove activity.allow_inline_create boolean node from DependencyInjection/Configuration
    - Remove activity.allow_inline_create translation entries from all language files

--- src/Controller/SystemConfigurationController.php
+++ src/Controller/SystemConfigurationController.php
@@ -519,10 +519,6 @@ final class SystemConfigurationController extends AbstractController
                         ->setLabel('allow_duplicate_number')
                         ->setType(YesNoType::class)
                         ->setTranslationDomain('system-configuration'),
-                    (new Configuration('activity.allow_inline_create'))
-                        ->setLabel('activity.allow_inline_create')
-                        ->setType(YesNoType::class)
-                        ->setTranslationDomain('system-configuration'),
                 ]),
             (new SystemConfigurationModel('user'))
                 ->setConfiguration([
