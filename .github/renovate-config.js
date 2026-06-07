dcad0ccc725abba2ef078c91565aa782c07657b5
Kevin Papst <kevin@kevinpapst.de>
2026-06-07 12:22:14 +0200

    add renovate bot

---
+++ .github/renovate-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    branchPrefix: 'renovate/',
+    username: 'renovate-release',
+    gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+    onboarding: false,
+    platform: 'github',
+    dryRun: 'full',
+};
