b81cdf4274123169aef76a2088fa0e0abd310b9a
Kevin Papst <kevin@kevinpapst.de>
2026-06-19 10:16:51 +0200

    remove event proposal

--- src/EventSubscriber/UserPreferenceSubscriber.php
+++ src/EventSubscriber/UserPreferenceSubscriber.php
@@ -14,7 +14,6 @@ use App\Controller\HomepageController;
 use App\Entity\User;
 use App\Entity\UserPreference;
 use App\Event\PrepareUserEvent;
-use App\Event\RegisterUserPreferencesEvent;
 use App\Event\UserPreferenceEvent;
 use App\Form\Type\CalendarViewType;
 use App\Form\Type\FavoriteMenuType;
@@ -162,7 +161,5 @@ final class UserPreferenceSubscriber implements EventSubscriberInterface
                 $user->addPreference($preference);
             }
         }
-
-        $this->eventDispatcher->dispatch(new RegisterUserPreferencesEvent($user));
     }
 }
