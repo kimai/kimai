affdc8d1693f3ddc34a823504023a9203a9ae395
Kevin Papst <kevin@kevinpapst.de>
2026-06-19 10:17:20 +0200

    bind default preferences when creating user

--- src/EventSubscriber/WorkContractPreferenceSubscriber.php
+++ src/EventSubscriber/WorkContractPreferenceSubscriber.php
@@ -10,7 +10,7 @@
 namespace App\EventSubscriber;
 
 use App\Entity\UserPreference;
-use App\Event\RegisterUserPreferencesEvent;
+use App\Event\UserCreateEvent;
 use App\WorkingTime\Calculator\WorkingTimeCalculatorDay;
 use App\WorkingTime\Mode\WorkingTimeModeNone;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -20,60 +20,33 @@ final class WorkContractPreferenceSubscriber implements EventSubscriberInterface
     public static function getSubscribedEvents(): array
     {
         return [
-            RegisterUserPreferencesEvent::class => ['onRegisterUserPreferences', 100]
+            UserCreateEvent::class => ['onRegisterUserPreferences', 100]
         ];
     }
 
-    public function onRegisterUserPreferences(RegisterUserPreferencesEvent $event): void
+    public function onRegisterUserPreferences(UserCreateEvent $event): void
     {
         $user = $event->getUser();
 
-        if (null === $user->getPreference(UserPreference::WORK_CONTRACT_TYPE)) {
-            $user->addPreference(new UserPreference(UserPreference::WORK_CONTRACT_TYPE, WorkingTimeModeNone::ID));
-        }
+        $prefs = [
+            UserPreference::WORK_CONTRACT_TYPE => WorkingTimeModeNone::ID,
+            WorkingTimeCalculatorDay::WORK_HOURS_MONDAY => 0,
+            WorkingTimeCalculatorDay::WORK_HOURS_TUESDAY => 0,
+            WorkingTimeCalculatorDay::WORK_HOURS_WEDNESDAY => 0,
+            WorkingTimeCalculatorDay::WORK_HOURS_THURSDAY => 0,
+            WorkingTimeCalculatorDay::WORK_HOURS_FRIDAY => 0,
+            WorkingTimeCalculatorDay::WORK_HOURS_SATURDAY => 0,
+            WorkingTimeCalculatorDay::WORK_HOURS_SUNDAY => 0,
+            UserPreference::PUBLIC_HOLIDAY_GROUP => null,
+            UserPreference::HOLIDAYS_PER_YEAR => 0.0,
+            'work_start_day' => null,
+            'work_last_day' => null,
+        ];
 
-        if (null === $user->getPreference(WorkingTimeCalculatorDay::WORK_HOURS_MONDAY)) {
-            $user->addPreference(new UserPreference(WorkingTimeCalculatorDay::WORK_HOURS_MONDAY, 0));
-        }
-
-        if (null === $user->getPreference(WorkingTimeCalculatorDay::WORK_HOURS_TUESDAY)) {
-            $user->addPreference(new UserPreference(WorkingTimeCalculatorDay::WORK_HOURS_TUESDAY, 0));
-        }
-
-        if (null === $user->getPreference(WorkingTimeCalculatorDay::WORK_HOURS_WEDNESDAY)) {
-            $user->addPreference(new UserPreference(WorkingTimeCalculatorDay::WORK_HOURS_WEDNESDAY, 0));
-        }
-
-        if (null === $user->getPreference(WorkingTimeCalculatorDay::WORK_HOURS_THURSDAY)) {
-            $user->addPreference(new UserPreference(WorkingTimeCalculatorDay::WORK_HOURS_THURSDAY, 0));
-        }
-
-        if (null === $user->getPreference(WorkingTimeCalculatorDay::WORK_HOURS_FRIDAY)) {
-            $user->addPreference(new UserPreference(WorkingTimeCalculatorDay::WORK_HOURS_FRIDAY, 0));
-        }
-
-        if (null === $user->getPreference(WorkingTimeCalculatorDay::WORK_HOURS_SATURDAY)) {
-            $user->addPreference(new UserPreference(WorkingTimeCalculatorDay::WORK_HOURS_SATURDAY, 0));
-        }
-
-        if (null === $user->getPreference(WorkingTimeCalculatorDay::WORK_HOURS_SUNDAY)) {
-            $user->addPreference(new UserPreference(WorkingTimeCalculatorDay::WORK_HOURS_SUNDAY, 0));
-        }
-
-        if (null === $user->getPreference(UserPreference::PUBLIC_HOLIDAY_GROUP)) {
-            $user->addPreference(new UserPreference(UserPreference::PUBLIC_HOLIDAY_GROUP, null));
-        }
-
-        if (null === $user->getPreference(UserPreference::HOLIDAYS_PER_YEAR)) {
-            $user->addPreference(new UserPreference(UserPreference::HOLIDAYS_PER_YEAR, 0.0));
-        }
-
-        if (null === $user->getPreference('work_start_day')) {
-            $user->addPreference(new UserPreference('work_start_day', null));
-        }
-
-        if (null === $user->getPreference('work_last_day')) {
-            $user->addPreference(new UserPreference('work_last_day', null));
+        foreach ($prefs as $prefName => $defaultValue) {
+            if ($user->getPreference($prefName) === null) {
+                $user->addPreference(new UserPreference($prefName, $defaultValue));
+            }
         }
     }
 }
