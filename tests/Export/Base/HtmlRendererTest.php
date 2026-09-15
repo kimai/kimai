957539b64a784593631d7673061a0ce4cc49404c
Ivan Rublev <github@ivanrublev.me>
2026-09-15 04:55:52 +0200

    fix(export): render print and PDF date/from/to columns in the query timezone
    Co-authored-by: Kaizero <noreply@kaizero.sh>

--- tests/Export/Base/HtmlRendererTest.php
+++ tests/Export/Base/HtmlRendererTest.php
@@ -10,9 +10,15 @@
 namespace App\Tests\Export\Base;
 
 use App\Activity\ActivityStatisticService;
+use App\Entity\Activity;
+use App\Entity\Customer;
+use App\Entity\Project;
+use App\Entity\Timesheet;
+use App\Entity\User;
 use App\Export\Base\HtmlRenderer;
 use App\Export\Base\RendererTrait;
 use App\Project\ProjectStatisticService;
+use App\Repository\Query\ExportQuery;
 use App\Tests\Export\Renderer\AbstractRendererTestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Group;
@@ -70,5 +76,43 @@ class HtmlRendererTest extends AbstractRendererTestCase
 
         $content = $response->getContent();
         self::assertIsString($content);
+    }
+
+    public function testRenderConvertsDateAndTimeIntoQueryTimezone(): void
+    {
+        /** @var Environment $twig */
+        $twig = $this->getContainer()->get(Environment::class);
+
+        $sut = $this->getAbstractRenderer($twig);
+
+        $customer = new Customer('Customer Name');
+        $project = new Project();
+        $project->setName('project name');
+        $project->setCustomer($customer);
+        $activity = new Activity();
+        $activity->setName('activity');
+        $activity->setProject($project);
+
+        $user = new User();
+        $user->setUserIdentifier('foo-bar');
+
+        $timesheet = new Timesheet();
+        $timesheet->setUser($user);
+        $timesheet->setProject($project);
+        $timesheet->setActivity($activity);
+        $timesheet->setBegin(new \DateTime('2026-08-21 01:00:00', new \DateTimeZone('Europe/Berlin')));
+        $timesheet->setEnd(new \DateTime('2026-08-21 02:00:00', new \DateTimeZone('Europe/Berlin')));
+
+        $query = new ExportQuery();
+        $query->setTimezone(new \DateTimeZone('America/New_York'));
+        $query->setBegin(new \DateTime('2026-08-20'));
+        $query->setEnd(new \DateTime('2026-08-20'));
+
+        $response = $sut->render([$timesheet], $query);
+        $content = $response->getContent();
+        self::assertIsString($content);
+
+        self::assertStringContainsString('Date (America/New_York)', $content);
+        self::assertStringNotContainsString('2026-08-21', $content);
     }
 }
